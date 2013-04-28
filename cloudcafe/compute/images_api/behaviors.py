d594cd9df449468e73564999ba6340302d4acd6f
Daryl Walleck <daryl.walleck@rackspace.com>
2013-04-28 01:01:23 -0500

    Refactored image behaviors
    * Modified polling methods to match standard CC style
    * Added create_active_image function
    
    Change-Id: I20f53550f5738f61d3ffea852e79b824111426a5

--- cloudcafe/compute/images_api/behaviors.py
+++ cloudcafe/compute/images_api/behaviors.py
@@ -16,6 +16,7 @@ limitations under the License.
 
 import time
 
+from cloudcafe.compute.common.datagen import rand_name
 from cloudcafe.compute.common.types import NovaImageStatusTypes as ImageStates
 from cloudcafe.compute.common.exceptions import ItemNotFound, \
     TimeoutException, BuildErrorException
@@ -23,66 +24,139 @@ from cloudcafe.compute.common.exceptions import ItemNotFound, \
 
 class ImageBehaviors(object):
 
-    def __init__(self, images_client, config):
+    def __init__(self, images_client, servers_client, config):
 
         self.config = config
         self.images_client = images_client
+        self.servers_client = servers_client
 
+    def wait_for_image_status(self, image_id, desired_status,
+                              interval_time=None, timeout=None):
+        """
+        @summary: Waits for a image to reach a desired status
+        @param image_id: The uuid of the image
+        @type image_id: String
+        @param desired_status: The desired final status of the image
+        @type desired_status: String
+        @param interval_time: The amount of time in seconds to wait
+                              between polling
+        @type interval_time: Integer
+        @param interval_time: The amount of time in seconds to wait
+                              before aborting
+        @type interval_time: Integer
+        @return: Response object containing response and the image
+                 domain object
+        @rtype: requests.Response
+        """
 
-    def wait_for_image_status(self, image_id, desired_status):
-        '''Polls image image_id details until status_to_wait_for is met.'''
-        image_response = self.images_client.get_image(image_id)
-        image_obj = image_response.entity
-        time_waited = 0
-        interval_time = self.config.image_status_interval
-        while (image_obj.status.lower() != desired_status.lower() and
-                       time_waited < self.config.snapshot_timeout):
-            image_response = self.images_client.get_image(image_id)
-            image_obj = image_response.entity
+        interval_time = interval_time or self.config.image_status_interval
+        timeout = timeout or self.config.snapshot_timeout
+        end_time = time.time() + timeout
 
-            if image_obj.status.lower() is ImageStates.ERROR.lower():
-                message = 'Snapshot failed. Image with uuid {0} entered ERROR status.'
-                raise BuildErrorException(message.format(image_id))
+        while time.time() < end_time:
+            resp = self.images_client.get_image(image_id)
+            image = resp.entity
 
+            if image.status.lower() == ImageStates.ERROR.lower():
+                raise BuildErrorException(
+                    'Build failed. Image with uuid %s entered ERROR status.'
+                    % image.id)
+
+            if image.status == desired_status:
+                break
             time.sleep(interval_time)
-            time_waited += interval_time
-        return image_response
+        else:
+            raise TimeoutException(
+                "wait_for_image_status ran for {0} seconds and did not "
+                "observe the image achieving the {1} status.".format(
+                    timeout, desired_status))
 
-    def wait_for_image_resp_code(self, image_id, response_code):
-        '''Polls image resp for the specified status code.'''
+        return resp
 
-        image_response = self.images_client.get_image(image_id)
-        image_obj = image_response.entity
-        time_waited = 0
-        interval_time = self.config.image_status_interval
-        while (image_response.status_code != response_code and
-                       image_obj.status.lower() != ImageStates.ERROR.lower() and
-                       time_waited < self.config.snapshot_timeout):
-            image_response = self.images_client.get_image(image_id)
-            image_obj = image_response.entity
+    def wait_for_image_resp_code(self, image_id, response_code,
+                                 interval_time=None, timeout=None):
+        """
+        @summary: Waits for a image to reach a desired status. Primarily
+                  as a polling mechanism to determine when a new image
+                  registers in Glance
+        @param image_id: The uuid of the image
+        @type image_id: String
+        @param response_code: The desired response code
+        @type response_code: String
+        @param interval_time: The amount of time in seconds to wait
+                              between polling
+        @type interval_time: Integer
+        @param interval_time: The amount of time in seconds to wait
+                              before aborting
+        @type interval_time: Integer
+        @return: Response object containing response and the image
+                 domain object
+        @rtype: requests.Response
+        """
+
+        interval_time = interval_time or self.config.image_status_interval
+        timeout = timeout or self.config.snapshot_timeout
+        end_time = time.time() + timeout
+
+        while time.time() < end_time:
+            resp = self.images_client.get_image(image_id)
+            if resp.status_code == response_code:
+                break
             time.sleep(interval_time)
-            time_waited += interval_time
-        return image_response
+        else:
+            raise TimeoutException(
+                "wait_for_image_resp_code ran for {0} seconds and did not "
+                "receive a response with status code {1}.".format(
+                    timeout, response_code))
+        return resp
 
-    def wait_for_image_to_be_deleted(self, image_id):
-        '''Waits for the image to be deleted. '''
+    def create_active_image(self, server_id):
+        """
+        @summary: Creates an image from a server and waits for
+                  the image to become active
+        @param server_id: The uuid of the image
+        @type server_id: String
+        @return: Response object containing response and the image
+                 domain object
+        @rtype: requests.Response
+        """
 
-        image_response = self.images_client.delete_image(image_id)
-        image_obj = image_response.entity
-        time_waited = 0
-        interval_time = self.config.image_status_interval
+        name = rand_name('image')
+        resp = self.servers_client.create_image(server_id, name)
+        assert resp.status_code == 202
 
-        try:
-            while (True):
-                image_response = self.images_client.get_image(image_id)
-                image_obj = image_response.entity
-                if time_waited > self.config.snapshot_timeout:
-                    raise TimeoutException("Timed out while deleting image id: %s" % image_id)
-                if image_obj.status.lower() == ImageStates.DELETED.lower():
-                    return
-                if image_obj.status.lower() != ImageStates.ERROR.lower():
-                    raise BuildErrorException("Image entered Error state while deleting, Image id : %s" % image_id)
-                time.sleep(interval_time)
-                time_waited += interval_time
-        except ItemNotFound:
-            pass
+        # Retrieve the image id from the response header
+        image_id = resp.headers['location'].rsplit('/')[-1]
+        resp = self.wait_for_image_status(image_id, ImageStates.ACTIVE)
+        return resp
+
+    def wait_for_image_to_be_deleted(self, image_id, interval_time=None,
+                                     timeout=None):
+        """
+        @summary: Waits for an image to be deleted
+        @param image_id: The uuid of the image
+        @type image_id: String
+        @param interval_time: The amount of time in seconds to wait
+                              between polling
+        @type interval_time: Integer
+        @param interval_time: The amount of time in seconds to wait
+                              before aborting
+        @type interval_time: Integer
+        """
+
+        interval_time = interval_time or self.config.image_status_interval
+        timeout = timeout or self.config.snapshot_timeout
+        end_time = time.time() + timeout
+
+        while time.time() < end_time:
+            try:
+                self.images_client.get_image(image_id)
+            except ItemNotFound:
+                break
+            time.sleep(interval_time)
+        else:
+            raise TimeoutException(
+                "wait_for_image_to_be_deleted ran for {0} seconds "
+                "and did not observe the image achieving the "
+                "{1} status.".format(
+                    timeout, ImageStates.DELETED))
