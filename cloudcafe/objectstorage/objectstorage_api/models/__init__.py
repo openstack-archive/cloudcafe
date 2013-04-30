aa4eb664f4462e2d0a95273259db4644abafdd9b
Carlos 'sn1p3r' Martinez <carlos.martinez@rackspace.com>
2013-04-30 14:51:20 -0500

    Subject: remove cloudfiles reference. Pep8 fixes
    removed cloudfiles reference in the models __init__.py
    removed extra blank line at end of file
    
    Change-Id: Ib7019e15a48020867bd33c2b46b0821272d60ffd

---
+++ cloudcafe/objectstorage/objectstorage_api/models/__init__.py
@@ -0,0 +1,16 @@
+'''
+<<<<<<< HEAD
+@summary: Domain Domain Level Objects specific to Cloud Files Object Storage
+=======
+<<<<<<< HEAD
+@summary: Domain Domain Level Objects specific to Object Storage
+=======
+@summary: Domain Domain Level Objects specific to Cloud Files Object Storage
+>>>>>>> a3641a0... pep8 fixes
+>>>>>>> Subject: remove cloudfiles reference
+@note: Primarily used to pass data between L{ccengine.providers},
+@note: L{ccengine.clients} and L{testrepo} Tests
+@note: Also used to perform JSON or XML Marshaling and Validation
+@note: Consumed by L{ccengine.providers}, L{ccengine.clients},
+@note: L{ccengine.common.connectors} and L{testrepo} Tests
+'''
