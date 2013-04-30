29445ce3d23be725fe4e6415211c320cb4e73049
Carlos 'sn1p3r' Martinez <carlos.martinez@rackspace.com>
2013-04-30 14:51:20 -0500

    pep8 fixes
    Change-Id: I85fc658287203357d3e87b174aee21533cfa676d

---
+++ cloudcafe/objectstorage/objectstorage_api/models/__init__.py
@@ -0,0 +1,8 @@
+'''
+@summary: Domain Domain Level Objects specific to Cloud Files Object Storage
+@note: Primarily used to pass data between L{ccengine.providers},
+@note: L{ccengine.clients} and L{testrepo} Tests
+@note: Also used to perform JSON or XML Marshaling and Validation
+@note: Consumed by L{ccengine.providers}, L{ccengine.clients},
+@note: L{ccengine.common.connectors} and L{testrepo} Tests
+'''
