7492dd704549091433c3d4680368cd22be6255a9
Carlos 'sn1p3r' Martinez <carlos.martinez@rackspace.com>
2013-05-10 15:15:17 -0500

    Subject: remove cloudfiles reference
    removed cloudfiles reference in the models __init__.py
    
    Change-Id: Ib7019e15a48020867bd33c2b46b0821272d60ffd

---
+++ cloudcafe/objectstorage/objectstorage_api/models/__init__.py
@@ -0,0 +1,12 @@
+'''
+<<<<<<< HEAD
+@summary: Domain Domain Level Objects specific to Object Storage
+=======
+@summary: Domain Domain Level Objects specific to Cloud Files Object Storage
+>>>>>>> a3641a0... pep8 fixes
+@note: Primarily used to pass data between L{ccengine.providers},
+@note: L{ccengine.clients} and L{testrepo} Tests
+@note: Also used to perform JSON or XML Marshaling and Validation
+@note: Consumed by L{ccengine.providers}, L{ccengine.clients},
+@note: L{ccengine.common.connectors} and L{testrepo} Tests
+'''
