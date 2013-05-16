6111e08a3aaa2998c92fefc7193085bd3e70c5f3
Sett Wai <sett.wai@gmail.com>
2013-05-16 18:03:59 -0500

    add Image API(Glance) models and their unit tests(pytest)
    Change-Id: Ibbe18b11d576881069a2171c6487a077a481f66d
    
    add Image API(Glance) client and its mock tests(HTTPretty)
    
    Change-Id: If78cff896599edaee72faf63d8753fd960910da4
    
    add common types and nuked __init__.py in tests folder
    
    Change-Id: Ie9d49f775f7a1c6e145fdbd0eaa4abc80230bf3f
    
    add Visibility enum type for Image API 2.0 models
    
    Change-Id: Ie258e09cb9ed4c7a50249cd911f503bb6b34dcaf
    
    add Images API v2.0 model
    
    fix pep8 violations
    
    add previously missing file
    
    Change-Id: Ic87634520a39b6ad05f7362a3c36e237e00b1840

---
+++ cloudcafe/images/common/types.py
@@ -0,0 +1,73 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
+class ImageStatus(object):
+    """
+    @summary: Types denoting an Image's status
+    @cvar QUEUED: Image identifier has been reserved
+    @type QUEUED: C{str}
+    @cvar SAVING: Image's raw data is currently being uploaded
+    @type SAVING: C{str}
+    @cvar ACTIVE: Image is active and available
+    @type ACTIVE: C{str}
+    @cvar KILLED: An error occured during upload of image data
+    @type KILLED: C{str}
+    @cvar PENDING_DELETE: Image data is not yet removed
+    @type PENDING_DELETE: C{str}
+    @cvar DELETED: Image information has been retained but image is no longer \
+            available
+    @type DELETED: C{str}
+    """
+
+    QUEUED = "QUEUED"
+    SAVING = "SAVING"
+    ACTIVE = "ACTIVE"
+    KILLED = "KILLED"
+    PENDING_DELETE = "PENDING_DELETE"
+    DELETED = "DELETED"
+
+
+class ImageDiskFormat(object):
+    """
+    @summary: Types denoting an Image's disk format.
+    """
+
+    RAW = "RAW"
+    VHD = "VHD"
+    VMDK = "VMDK"
+    VDI = "VDI"
+    ISO = "ISO"
+    QCOW2 = "QCOW2"
+    AKI = "AKI"
+    AMI = "AMI"
+
+
+class ImageContainerFormat(object):
+    """
+    @summary: Types denoting an Image's container format.
+    """
+
+    BARE = "BARE"
+    OVF = "OVF"
+    AKI = "AKI"
+    ARI = "ARI"
+    AMI = "AMI"
+
+
+class ImageVisibility(object):
+    PUBLIC = 'PUBLIC'
+    PRIVATE = 'PRIVATE'
---
+++ tests/cloudcafe/images/v1_0/models/image_test.py
@@ -0,0 +1,149 @@
+import re
+from datetime import datetime
+from unittest import TestCase
+
+from cloudcafe.images.v1_0.models.image import Image
+from cloudcafe.images.v1_0.models.member import Member
+
+
+class ImageTest(TestCase):
+    """@summary: Testing the behaviors of the image model..."""
+
+    def setUp(self):
+        self.raw_str = '{"images": ' \
+                       '[{"status": "active", ' \
+                       '"name": "precise", ' \
+                       '"deleted": false, ' \
+                       '"container_format": "bare", ' \
+                       '"created_at": "2013-04-29T19:32:56", ' \
+                       '"disk_format": "qcow2", ' \
+                       '"updated_at": "2013-04-29T19:32:56", ' \
+                       '"properties": {}, ' \
+                       '"min_disk": 0, ' \
+                       '"protected": false, ' \
+                       '"id": "46fd5b5c-b925-4316-a878-63cbbe7f0030", ' \
+                       '"checksum": null, ' \
+                       '"owner": "bd7531a57d3a47538fae1b89c169b293", ' \
+                       '"is_public": true, ' \
+                       '"deleted_at": null, ' \
+                       '"min_ram": 0, ' \
+                       '"size": 252116992}]}'
+
+        self.image_one = Image(
+            id="46fd5b5c-b925-4316-a878-63cbbe7f0030",
+            status="active",
+            name="precise",
+            deleted=False,
+            container_format="bare",
+            created_at=datetime.strptime("2013-04-29T19:32:56",
+                                         '%Y-%m-%dT%H:%M:%S'),
+            disk_format="qcow2",
+            updated_at=datetime.strptime("2013-04-29T19:32:56",
+                                         '%Y-%m-%dT%H:%M:%S'),
+            owner="bd7531a57d3a47538fae1b89c169b293",
+            protected=False,
+            min_ram=0,
+            checksum=None,
+            min_disk=0,
+            is_public=True,
+            deleted_at=None,
+            properties={},
+            size=252116992)
+
+        self.image_two = Image(
+            id="c7dd539e-5077-49e8-bc4d-0359ba051122",
+            status="active",
+            name="precise",
+            deleted="False",
+            container_format="cirros",
+            created_at=datetime.today(),
+            disk_format="qcow2",
+            updated_at=datetime.today(),
+            owner="bd7531a57d3a47538fae1b89c169b293",
+            protected="False",
+            min_ram=0,
+            checksum="",
+            min_disk=0,
+            is_public="True",
+            deleted_at=datetime.today(),
+            properties={},
+            size=252116992)
+
+        self._dict = {
+            'id': self.image_one.id,
+            'status': self.image_one.status,
+            'name': self.image_one.name,
+            'deleted': self.image_one.deleted,
+            'container_format': self.image_one.container_format,
+            'created_at': "2013-04-29T19:32:56",
+            'disk_format': self.image_one.disk_format,
+            'updated_at': "2013-04-29T19:32:56",
+            'owner': self.image_one.owner,
+            'protected': self.image_one.protected,
+            'min_ram': self.image_one.min_ram,
+            'checksum': self.image_one.checksum,
+            'min_disk': self.image_one.min_disk,
+            'is_public': self.image_one.is_public,
+            'deleted_at': self.image_one.deleted_at,
+            'properties': self.image_one.properties,
+            'size': self.image_one.size
+        }
+
+    def test_positive_equality_of_images(self):
+        assert self.image_one.__eq__(self.image_one)
+        assert not self.image_one.__eq__(self.image_two)
+
+    def test_negative_equality_of_images(self):
+        assert self.image_one.__ne__(self.image_two)
+        assert not self.image_one.__ne__(self.image_one)
+
+    def test_dict_to_image(self):
+        assert Image._dict_to_obj(self._dict).__eq__(self.image_one)
+
+    def test_image_string_representations(self):
+        regex = r"""(?x)\[(\s?\w*\:\s(\d{4}-\d{2}-\d{2}\s\d{2}:
+            \d{2}:\d{2}\.\d{6}|[\w\d\-\{\}]*)\,?)*\]"""
+        matches = re.match(regex, self.image_one.__repr__())
+        assert matches is not None
+
+    def test_json_to_image(self):
+        assert len(Image._json_to_obj(self.raw_str)) == 1
+        assert Image._json_to_obj(self.raw_str)[0].__eq__(self.image_one)
+
+    def test_add_member_to_an_image(self):
+        member1 = Member(member_id='1')
+
+        self.image_one.add_member(member1)
+
+        assert len(self.image_one.members_list) == 1
+        assert member1 in self.image_one.members_list
+
+    def test_remove_member_from_an_image(self):
+        member1 = Member(member_id='1')
+        member2 = Member(member_id='2')
+
+        self.image_one.add_member(member1)
+        self.image_one.add_member(member2)
+        assert len(self.image_one.members_list) == 2
+
+        self.image_one.delete_member(member1)
+
+        assert len(self.image_one.members_list) == 1
+        assert member1 not in self.image_one.members_list
+        assert member2 in self.image_one.members_list
+
+    def test_replace_members_list_for_an_image(self):
+        member1 = Member(member_id='1')
+        member2 = Member(member_id='2')
+        member3 = Member(member_id='3')
+
+        members_list = [member1, member2]
+        image = self.image_one
+        image.members_list = members_list
+
+        image.replace_members_list([member3])
+
+        assert len(image.members_list) == 1
+        assert member1 not in image.members_list
+        assert member2 not in image.members_list
+        assert member3 in image.members_list
