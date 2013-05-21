4f153c9b3718ae66386e0d9085aa3e6541d83948
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-05-21 13:38:18 -0500

    [#] adds new extensions api model.
    [] cleans up extensions api and add extension test.
    
    Change-Id: I4f0691b5dfd2302c3414b3c8d4b881b1624063e6

---
+++ cloudcafe/identity/v2_0/extensions_api/models/responses/extensions.py
@@ -0,0 +1,119 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+from cloudcafe.identity.v2_0.base import \
+    BaseIdentityModel, BaseIdentityListModel
+
+
+class Extensions(BaseIdentityModel):
+    def __init__(self, values=None):
+        """
+        Models a extensions object returned by keystone
+        """
+        self.values = values
+
+    @classmethod
+    def _dict_to_obj(cls, json_dict):
+        extensions = Extensions()
+        extensions.values = Values._list_to_obj(
+            json_dict.get('extensions'))
+
+        return extensions
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+        return cls._dict_to_obj(json_dict.get('extensions'))
+
+
+class Values(BaseIdentityListModel):
+    def __init__(self, values=None):
+        '''
+        Models a list of values returned by keystone
+        '''
+        super(Values, self).__init__()
+        self.extend(values)
+
+    @classmethod
+    def _list_to_obj(self, value_dict_list):
+        values = Values([])
+        for value_dict in value_dict_list:
+            value = Value._dict_to_obj(value_dict)
+            values.append(value)
+
+        return values
+
+
+class Value(BaseIdentityModel):
+    def __init__(self, updated=None, name=None, links=None, namespace=None,
+                 alias=None, description=None):
+        '''
+        Models a value object returned by keystone
+        '''
+        self.updated = updated
+        self.name = name
+        self.links = links
+        self.namespace = namespace
+        self.alias = alias
+        self.description = description
+
+    @classmethod
+    def _dict_to_obj(cls, json_dict):
+        value = Value(updated=json_dict.get('updated'),
+                      name=json_dict.get('name'),
+                      namespace=json_dict.get('namespace'),
+                      alias=json_dict.get('alias'),
+                      description=json_dict.get('description'),
+                      links=(Links._list_to_obj(json_dict.get('links'))))
+
+        return value
+
+
+class Links(BaseIdentityListModel):
+    def __init__(self, links=None):
+        '''
+        Models a list of links returned by keystone
+        '''
+        super(Links, self).__init__()
+        self.extend(links)
+
+    @classmethod
+    def _list_to_obj(self, link_dict_list):
+        links = Links([])
+        for link_dict in link_dict_list:
+            link = Link._dict_to_obj(link_dict)
+            links.append(link)
+
+        return links
+
+
+class Link(BaseIdentityModel):
+    def __init__(self, href=None, type_=None, rel=None):
+        '''
+        Models a link object returned by keystone
+        '''
+        self.href = href
+        self.type_ = type_
+        self.rel = rel
+
+    @classmethod
+    def _dict_to_obj(cls, json_dict):
+        link = Link(href=json_dict.get('href'),
+                    type_=json_dict.get('type'),
+                    rel=json_dict.get('rel'))
+
+        return link
---
+++ test/cloudcafe/identity/v2_0/extensions_api/models/responses/extensions_test.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+from cloudcafe.identity.v2_0.extensions_api.models.responses.extensions import Extensions, Value, Values, Link, Links
+
+
+class ExtensionsTest(TestCase):
+    def setUp(self):
+        self.href = "URL (e.g.:https://github.com/openstack/identity-api)"
+        self.type = "TEXT_HTML (e.g.:text/html)"
+        self.rel = "DESCRIBED_BY (e.g.: described by)"
+        self.updated_date = "UPDATED_DATE (e.g.:2011-08-19T13:25:27-06:00)"
+        self.name = "KEYSTONE_ADMIN_NAME (E.G.: Openstack Keystone Admin)"
+
+        self.dict_for_link = {"href": self.href, "type": self.type, "rel": self.rel}
+        self.name_space = "NAME_SPACE(e.g.: http://docs.openstack.org/identity/api/ext/OS-KSADM/v1.0)"
+        self.alias = "ALIAS (e.g.: OS-KSADM)"
+        self.description = "DESCRIPTION (e.g.: Openstack extensions to Keystone v2.0 API enabling Admin Operations.)"
+        self.dict_for_value = {
+            'updated': "UPDATED_DATE (e.g.:2011-08-19T13:25:27-06:00)",
+            'name': "KEYSTONE_ADMIN_NAME (E.G.: Openstack Keystone Admin)",
+            'links': [{"href": "URL (e.g.:https://github.com/openstack/identity-api)",
+                       "type": "TEXT_HTML (e.g.:text/html)",
+                       "rel": "DESCRIBED_BY (e.g.: described by)"}],
+            'namespace': "NAME_SPACE(e.g.: http://docs.openstack.org/identity/api/ext/OS-KSADM/v1.0)",
+            'alias': "ALIAS (e.g.: OS-KSADM)",
+            'description': "DESCRIPTION (e.g.: Openstack extensions to Keystone v2.0 API enabling Admin Operations.)"
+        }
+
+        self.link = Link(href=self.href, type_=self.type, rel=self.rel)
+        self.links = Links(links=[self.link])
+
+        self.link_dict_list = [self.dict_for_link]
+        self.value_dict_list = [self.dict_for_value]
+
+        self.value = Value(updated=self.updated_date,
+                           name=self.name,
+                           links=[self.link],
+                           namespace=self.name_space,
+                           alias=self.alias,
+                           description=self.description)
+
+        self.values_list = Values(values=[self.value])
+
+        self.extensions = Extensions(values=self.values_list)
+        self.dict_for_extensions = {'extensions': [self.dict_for_value]}
+
+    def test_dict_to_obj(self):
+        assert Extensions._dict_to_obj(self.dict_for_extensions) == self.extensions
+        assert Link._dict_to_obj(self.dict_for_link) == self.link
+        assert Value._dict_to_obj(self.dict_for_value) == self.value
+
+    def test_list_to_obj(self):
+        assert Links._list_to_obj(self.link_dict_list) == self.links
+        assert Values._list_to_obj(self.value_dict_list) == self.values_list
