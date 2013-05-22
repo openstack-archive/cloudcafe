d0125e4c97e305393a5ff52efd7d095ad165b07c
Sett Wai <sett.wai@gmail.com>
2013-05-22 14:28:57 -0500

    fixes based on code review recommendations
    Previous commits:
    --------
    finish v2.0 model and minor fix in v1.0 model
    
    add images v2.0 models, unittest and test data
    
    move tests/* into cloudcafe/metatests/ to avoid namespace issues
    
    fleshed out v2.0. model and unittests
    
    Change-Id: I86f339e6a306b44d5c667819ac1af402e86a362b

--- cloudcafe/metatests/conftest.py
+++ cloudcafe/metatests/conftest.py
@@ -1,8 +1,5 @@
 # Configure path for py.test
 import os
-import sys
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
-print sys.path
 
 os.environ["CCTNG_CONFIG_FILE"] = os.path.join(
     os.path.dirname(__file__),
