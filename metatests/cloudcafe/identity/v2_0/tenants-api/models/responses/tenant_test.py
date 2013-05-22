7d1401114cbbcc3418f60c8a67fc4721f57945b1
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-05-22 14:10:25 -0500

    New Tenants api model
    1. Added a role model and unit test.
    2. Moved all unit tests to a metatests package.
    
    Change-Id: Id660ff4b5be7020e90786eed5d5405d78ca9737e

--- metatests/cloudcafe/identity/v2_0/tenants-api/models/responses/tenant_test.py
+++ metatests/cloudcafe/identity/v2_0/tenants-api/models/responses/tenant_test.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 from unittest import TestCase
 from cloudcafe.identity.v2_0.tenants_api.models.responses.tenant import \
     Tenant, TenantsLink, Tenants
@@ -27,7 +43,8 @@ class TenantTest(TestCase):
                                      '"name": "TENANT_NAME", ' \
                                      '"description": "TENANT_DESCRIPTION"}}'
 
-        self.expected_tenant_link = TenantsLink(href="HREF", type_="TYPE", rel="REL")
+        self.expected_tenant_link = TenantsLink(href="HREF", type_="TYPE",
+                                                rel="REL")
         self.tenant_list_dict = [self.tenant_dict]
         self.expected_tenants = Tenants(tenants=[self.expected_tenant])
 
@@ -41,8 +58,11 @@ class TenantTest(TestCase):
         assert self.expected_tenant == Tenant._dict_to_obj(self.tenant_dict)
 
     def test_json_to_obj(self):
-        assert self.expected_tenant == Tenant._json_to_obj(self.tenant_serialized_str)
-        assert self.expected_tenants == Tenants._json_to_obj(self.tenants_serialized_str)
+        assert self.expected_tenant == Tenant._json_to_obj(
+            self.tenant_serialized_str)
+        assert self.expected_tenants == Tenants._json_to_obj(
+            self.tenants_serialized_str)
 
     def test_list_to_obj(self):
-        assert self.expected_tenants == Tenants._list_to_obj(self.tenant_list_dict)
+        assert self.expected_tenants == Tenants._list_to_obj(
+            self.tenant_list_dict)
