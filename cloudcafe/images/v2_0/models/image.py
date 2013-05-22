dd18a1c7fe881fbe155248bec4f932849459800d
Sett Wai <sett.wai@gmail.com>
2013-05-22 14:28:57 -0500

    add images v2.0 models, unittest and test data
    Change-Id: Idc37e8899adcad7be20eb9e8136059ea02da10e5
    
    move tests/* into cloudcafe/metatests/ to avoid namespace issues
    
    Change-Id: Ib979d9b10006e552ef6c6a10498e7d18e6ed9882
    
    fleshed out v2.0. model and unittests
    
    Change-Id: I86f339e6a306b44d5c667819ac1af402e86a362b

---
+++ cloudcafe/images/v2_0/models/image.py
@@ -0,0 +1,114 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.engine.models.base import AutoMarshallingModel
+from cloudcafe.compute.common.equality_tools import EqualityTools
+from datetime import datetime
+import json
+
+
+class Image(AutoMarshallingModel):
+    """@Summary Openstack Images(Glance) API 2.0 model"""
+
+    def __init__(self, id_=None, name=None, visibility=None,
+                 status=None, protected=None, tags=None, checksum=None,
+                 size=None, created_at=None, updated_at=None, file_=None,
+                 self_=None, schema=None, additional_properties=None,
+                 container_format=None, disk_format=None, min_disk=None,
+                 min_ram=None):
+
+        self.id_ = id_
+        self.name = name
+        self.visibility = visibility
+        self.status = status
+        self.protected = protected
+        self.tags = tags
+        self.checksum = checksum
+        self.size = size
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.file_ = file_
+        self.self_ = self_
+        self.schema = schema
+        self.additonal_properties = additional_properties
+        self.container_format = container_format
+        self.disk_format = disk_format
+        self.min_disk = min_disk
+        self.min_ram = min_ram
+
+    def __eq__(self, other):
+        """
+        @summary: Overrides the default equals
+        @param other: Image object to compare with
+        @type other: Image
+        @return: True if Image objects are equal, False otherwise
+        @rtype: bool
+        """
+        return EqualityTools.are_objects_equal(self, other)
+
+    def __ne__(self, other):
+        """
+        @summary: Overrides the default not-equals
+        @param other: Image object to compare with
+        @type other: Image
+        @return: True if Image objects are not equal, False otherwise
+        @rtype: bool
+        """
+        return not self == other
+
+    def __repr__(self):
+        values = []
+        for prop in self.__dict__:
+            if prop in ['created_at', 'updated_at']:
+                date_property = self.__dict__[prop]
+                self.__dict__[prop] = 'None' or \
+                    date_property.strftime('%Y-%m-%dT%H:%M:%S')
+            values.append("%s: %s" % (prop, self.__dict__[prop]))
+        return '[' + ', '.join(values) + ']'
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        serialized_str.replace('false', 'False')
+        serialized_str.replace('true', 'True')
+        json_dict = json.loads(serialized_str)
+
+        if 'images' in json_dict.keys():
+            images = []
+            for image_dict in json_dict['images']:
+                images.append(cls._dict_to_obj(image_dict))
+            return images
+
+    @classmethod
+    def _dict_to_obj(cls, json_dict):
+        """@summary: Processing dates in converting string to date objects"""
+
+        for date_key in ['created_at', 'updated_at']:
+            if json_dict[date_key]:
+                json_dict[date_key] = datetime.strptime(json_dict[date_key],
+                                                        '%Y-%m-%dT%H:%M:%S')
+        return Image(**json_dict)
+
+    def _obj_to_json(self):
+        pass
+
+    @classmethod
+    def _xml_to_obj(cls, serialized_str):
+        raise NotImplementedError("Glance does not serve XML-formatted \
+                                  resources.")
+
+    def _obj_to_xml(self):
+        raise NotImplementedError("Glance does not serve XML-formatted \
+                                  resources.")
---
+++ cloudcafe/metatests/images/v2_0/models/image_test.py
@@ -0,0 +1,61 @@
+import os
+from copy import deepcopy
+from datetime import datetime
+from unittest import TestCase
+
+from cloudcafe.images.common.types import ImageVisibility, \
+    ImageStatus, ImageContainerFormat, ImageDiskFormat
+from cloudcafe.images.v2_0.models.image import Image
+
+
+class TestImage(TestCase):
+
+    @classmethod
+    def setup_class(cls):
+        cls.raw_image_str = open(os.path.join(
+            os.path.dirname(__file__), '../data/image.json')).read()
+        cls.raw_images_str = open(os.path.join(
+            os.path.dirname(__file__), '../data/images.json')).read()
+
+        cls.image_obj = Image(
+            id_='21c697d1-2cc5-4a45-ba50-61fab15ab9b7',
+            name='cirros-0.3.1-x86_64-uec-ramdisk',
+            visibility=ImageVisibility.PUBLIC,
+            status=ImageStatus.ACTIVE,
+            protected=False,
+            tags=[],
+            checksum='69c33642f44ca552ba4bb8b66ad97e85',
+            size=3714968,
+            created_at=datetime.strptime('2013-05-22T14:24:36Z',
+                                         '%Y-%m-%dT%H:%M:%SZ'),
+            updated_at=datetime.strptime('2013-05-22T14:24:36Z',
+                                         '%Y-%m-%dT%H:%M:%SZ'),
+            file_='/v2/images/21c697d1-2cc5-4a45-ba50-61fab15ab9b7/file',
+            self_='/v2/images/21c697d1-2cc5-4a45-ba50-61fab15ab9b7',
+            schema='/v2/schemas/image',
+            container_format=ImageContainerFormat.ARI,
+            disk_format=ImageDiskFormat.ARI,
+            min_disk=0,
+            min_ram=0
+        )
+
+    def test_positive_equality(self):
+        duplicate_obj = deepcopy(self.image_obj)
+        assert duplicate_obj == self.image_obj
+
+    def test_negative_equality(self):
+        different_obj = deepcopy(self.image_obj)
+        different_obj.created_at = datetime.now()
+        different_obj.updated_at = datetime.now()
+        different_obj.name = 'cirros-fake'
+
+        assert different_obj != self.image_obj
+
+    def test_string_representation(self):
+        pass
+
+    def test_serialization_to_json(self):
+        pass
+
+    def test_deserialization_from_json(self):
+        pass
