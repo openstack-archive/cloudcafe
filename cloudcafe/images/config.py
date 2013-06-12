a6e078808e0c703a8d76fbf32f7c9fa860c08ceb
Sett Wai <sett.wai@gmail.com>
2013-06-12 16:07:54 +0200

    add config provider and examples for images api
    - edit reference.xml.config to reflect xml as marshalling method
    
    Change-Id: I6a6392dfc306de0b57adc8006df11f3b1ce983d5

---
+++ cloudcafe/images/config.py
@@ -0,0 +1,42 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cloudcafe.common.models.configuration import ConfigSectionInterface
+
+
+class ImagesConfig(ConfigSectionInterface):
+
+    SECTION_NAME = 'images'
+
+    @property
+    def base_url(self):
+        """Base URL where Images API is reached"""
+        return self.get('base_url')
+
+    @property
+    def api_version(self):
+        """API Version to use"""
+        return self.get('api_version')
+
+    @property
+    def primary_image(self):
+        """Primary image ID"""
+        return self.get('primary_image')
+
+    @property
+    def secondary_image(self):
+        """Secondary image ID"""
+        return self.get('secondary_image')
