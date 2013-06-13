96ead0dfe3574ba7073aed9fab9d12a8f0713e61
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-06-13 16:48:39 +0200

    Idendity Configuration:
    1. Added a new config file to identity package.
    2. Added token authentification properties.
    
    Change-Id: Ifd06334fca9e0015430131328dff06eda5cac86b

---
+++ cloudcafe/identity/config.py
@@ -0,0 +1,56 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from cloudcafe.common.models.configuration import ConfigSectionInterface
+
+
+class IdentityEndpointConfig(ConfigSectionInterface):
+    SECTION_NAME = 'identity_endpoint'
+
+    @property
+    def endpoint_url(self):
+        return self.get("endpoint_url")
+
+
+class IdentityTokenConfig(ConfigSectionInterface):
+    SECTION_NAME = 'token_api'
+
+    @property
+    def serialize_format(self):
+        return self.get("serialize_format")
+
+    @property
+    def deserialize_format(self):
+        return self.get("deserialize_format")
+
+    @property
+    def version(self):
+        return self.get("version")
+
+    @property
+    def username(self):
+        return self.get("username")
+
+    @property
+    def password(self):
+        return self.get("password")
+
+    @property
+    def tenant_name(self):
+        return self.get("tenant_name")
+
+    @property
+    def authentication_endpoint(self):
+        return self.get("authentication_endpoint")
