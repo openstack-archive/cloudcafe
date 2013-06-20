911617eab021bdf09815d8e6e5e641e794308851
Sumanth Nagadavalli <sumanth.nagadavalli@rackspace.com>
2013-06-20 16:35:53 +0530

    Porting existing compute tests to metatests
    1. Ported all compute unittests to metatests
    2. Ported all compute client integration tests to
       metatests
    
    Change-Id: Ia80e2afcb1cca3921338392ed3cc6c0489eb1a2c

--- metatests/cloudcafe/compute/quotas/client/test_quotas_client.py
+++ metatests/cloudcafe/compute/quotas/client/test_quotas_client.py
@@ -18,12 +18,12 @@ import unittest2 as unittest
 from httpretty import HTTPretty
 from cloudcafe.compute.quotas_api.client import QuotasClient
 
-from cloudcafe.compute.tests.integration.fixtures import IntegrationTestFixture
-from cloudcafe.compute.tests.integration.quotas.responses\
+from metatests.cloudcafe.compute.fixtures import ClientTestFixture
+from metatests.cloudcafe.compute.quotas.client.responses \
     import QuotasMockResponse
 
 
-class QuotasClientTest(IntegrationTestFixture):
+class QuotasClientTest(ClientTestFixture):
 
     @classmethod
     def setUpClass(cls):
@@ -33,10 +33,10 @@ class QuotasClientTest(IntegrationTestFixture):
             auth_token=cls.AUTH_TOKEN,
             serialize_format=cls.FORMAT,
             deserialize_format=cls.FORMAT)
-        cls.quotas_uri = "{0}/os-quota-sets/{1}".\
-            format(cls.COMPUTE_API_ENDPOINT, cls.TENANT_ID)
-        cls.default_quotas_uri = "{0}/os-quota-sets/{1}/defaults".\
-            format(cls.COMPUTE_API_ENDPOINT, cls.TENANT_ID)
+        cls.quotas_uri = ("{0}/os-quota-sets/{1}".
+                          format(cls.COMPUTE_API_ENDPOINT, cls.TENANT_ID))
+        cls.default_quotas_uri = ("{0}/os-quota-sets/{1}/defaults".
+                                  format(cls.COMPUTE_API_ENDPOINT, cls.TENANT_ID))
         cls.mock_response = QuotasMockResponse(cls.FORMAT)
 
     def test_get_quota(self):
@@ -45,9 +45,8 @@ class QuotasClientTest(IntegrationTestFixture):
         actual_response = self.quotas_client.get_quota(self.TENANT_ID)
 
         self._assert_default_headers_in_request(HTTPretty.last_request)
-        self.assertEqual(200, actual_response.status_code)
-        self.assertEqual(self.mock_response._get_quota(),
-                         actual_response.content)
+        assert 200 == actual_response.status_code
+        assert self.mock_response._get_quota() == actual_response.content
 
     def test_get_default_quota(self):
         HTTPretty.register_uri(HTTPretty.GET, self.default_quotas_uri,
@@ -55,30 +54,23 @@ class QuotasClientTest(IntegrationTestFixture):
         actual_response = self.quotas_client.get_default_quota(self.TENANT_ID)
 
         self._assert_default_headers_in_request(HTTPretty.last_request)
-        self.assertEqual(200, actual_response.status_code)
-        self.assertEqual(self.mock_response._get_quota(),
-                         actual_response.content)
+        assert 200 == actual_response.status_code
+        assert self.mock_response._get_quota() == actual_response.content
 
     def test_update_quota(self):
         HTTPretty.register_uri(HTTPretty.PUT, self.quotas_uri,
                                body=self.mock_response._get_quota())
-        actual_response = self.quotas_client.\
-            update_quota(self.TENANT_ID, security_groups=45)
+        actual_response = (self.quotas_client.
+            update_quota(self.TENANT_ID, security_groups=45))
 
-        expected_request_body = '{"quota_set": {"security_group_rules": 45}}'
+        expected_request_body = '{"quota_set": {"security_groups": 45}}'
         self._assert_default_headers_in_request(HTTPretty.last_request)
-        self.assertEqual(200, actual_response.status_code)
-        self.assertEqual(HTTPretty.last_request.body,
-                         expected_request_body)
-        self.assertEqual(self.mock_response._get_quota(),
-                         actual_response.content)
+        assert 200 == actual_response.status_code
+        assert HTTPretty.last_request.body == expected_request_body
+        assert self.mock_response._get_quota() == actual_response.content
 
     def test_delete_quota(self):
         HTTPretty.register_uri(HTTPretty.DELETE, self.quotas_uri, status=202)
         actual_response = self.quotas_client.delete_quota(self.TENANT_ID)
 
-        self.assertEqual(202, actual_response.status_code)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert 202 == actual_response.status_code
