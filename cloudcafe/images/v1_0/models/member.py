783f9dbebe19dcbb86e6c1fb8eb7924450c10b90
Sett Wai <sett.wai@gmail.com>
2013-07-01 16:15:14 +0200

    changes to make image membership tests pass
    Needed by
    Change-Id: I270ad24c4d82bc31fb5b07ea05bc84458844143e
    
     - implemented member model deserialization
     - updated relevant images client calls
     - fixed outdated/incorrect import user.py
     - fixed tenants deserialization
    
    Change-Id: I4a892c001ab312000ce59da59c73d3a75316963e

--- cloudcafe/images/v1_0/models/member.py
+++ cloudcafe/images/v1_0/models/member.py
@@ -1,7 +1,20 @@
+import json
 from cafe.engine.models.base import AutoMarshallingModel
 
 
 class Member(AutoMarshallingModel):
-    def __init__(self, member_id=None, shared_images=None):
+    def __init__(self, member_id=None, shared_images=None, can_share=None):
         self.member_id = member_id
         self.shared_images = shared_images
+        self.can_share = can_share
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+
+        if 'members' in json_dict.keys():
+            members = []
+            members.extend([Member(**m) for m in json_dict['members']])
+            return members
+        else:
+            return Member(**json_dict)
