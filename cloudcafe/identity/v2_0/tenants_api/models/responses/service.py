1f434eeab44c7f48630eb38bec38ecfa3cffcb17
Charles Kimpolo <clmkimpo@thoughtworks.com>
2013-07-25 11:56:00 +0200

    Tenants API Service Model - Service Tests Parity.
     Added a service model/test to the tenants-api response models.
    
    Change-Id: I8f940ec560e12e5c7aaebaf074c7bc02c1a67fbb

---
+++ cloudcafe/identity/v2_0/tenants_api/models/responses/service.py
@@ -0,0 +1,76 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import json
+
+from cloudcafe.identity.v2_0.common.models.base import BaseIdentityListModel, \
+    BaseIdentityModel
+from cloudcafe.identity.v2_0.common.models.constants import AdminExtensions
+
+_admin_extensions = AdminExtensions.OS_KS_ADM
+
+
+class Services(BaseIdentityListModel):
+    def __init__(self, services=None):
+        """
+        An object that represents a services response object.
+        """
+        super(Services, self).__init__()
+        self.extend(services or [])
+
+    @classmethod
+    def _list_to_obj(cls, service_dict_list):
+        services = Services()
+        for service_dict in service_dict_list:
+            service = Service._dict_to_obj(service_dict)
+            services.append(service)
+        return services
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+        service_dict_list = json_dict.get(
+            '{0}:services'.format(_admin_extensions))
+        return cls._list_to_obj(service_dict_list)
+
+
+class Service(BaseIdentityModel):
+    def __init__(self, id_=None, name=None, type_=None, description=None):
+        """
+        An object that represents a service response object.
+        """
+        super(Service, self).__init__()
+        self.id_ = id_
+        self.name = name
+        self.type_ = type_
+        self.description = description
+
+    def _obj_to_json(self):
+        json_dict = {"service": {"name": self.name,
+                                 "type": self.type_,
+                                 "description": self.description}}
+        return json.dumps(json_dict)
+
+    @classmethod
+    def _dict_to_obj(cls, json_dict):
+        service = Service(name=json_dict.get("name"),
+                          type_=json_dict.get("type"),
+                          description=json_dict.get("description"))
+        return service
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+        return cls._dict_to_obj(json_dict.get("service"))
---
+++ metatests/cloudcafe/identity/v2_0/tenants_api/models/responses/service_test.py
@@ -0,0 +1,54 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+from cloudcafe.identity.v2_0.tenants_api.models.responses.service \
+    import Service, Services
+
+
+class TestService(object):
+    @classmethod
+    def setup_class(cls):
+        cls.name = "keystone-test"
+        cls.type = "identity-test"
+        cls.description = "Keystone Identity Service Test"
+        cls.service_dict = {"name": cls.name,
+                            "type": cls.type,
+                            "description": cls.description}
+        cls.expected_service = Service(name=cls.name,
+                                       type_=cls.type,
+                                       description=cls.description)
+        cls.json_dict = json.dumps({"service": cls.service_dict})
+        cls.expected_services = Services(services=[cls.expected_service])
+        cls.service_dict_list = [cls.service_dict]
+        cls.admin_ext = "OS-KSADM"
+        cls.services_json_dict = json.dumps({
+            "{0}:services".format(cls.admin_ext): cls.service_dict_list})
+
+    def test_dict_to_obj(self):
+        assert self.expected_service == Service._dict_to_obj(self.service_dict)
+
+    def test_json_to_obj(self):
+        assert self.expected_service == Service._json_to_obj(self.json_dict)
+        assert self.expected_services == Services._json_to_obj(
+            self.services_json_dict)
+
+    def test_obj_json(self):
+        assert self.json_dict == self.expected_service._obj_to_json()
+
+    def test_list_to_obj(self):
+        self.expected_services == Services._list_to_obj(
+            self.service_dict_list)
