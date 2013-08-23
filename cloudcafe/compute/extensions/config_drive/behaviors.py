a67c090f514954ca5c399e452d12d70b4ff60c40
ivo5307 <ivo.vasev@RACKSPACE.COM>
2013-08-23 09:50:55 -0500

    Added behaviour and model for first config drive openstack metadata
       * Added Model
       * Added client calling deserialization
    
    Change-Id: Idfc8907bed96041b85a816e3ecf34cbfe00d1e34

---
+++ cloudcafe/compute/extensions/config_drive/behaviors.py
@@ -0,0 +1,43 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import time
+
+from cafe.engine.clients.remote_instance.instance_client import \
+    InstanceClientFactory
+from cloudcafe.compute.common.types import InstanceAuthStrategies
+from cloudcafe.compute.common.types import NovaServerStatusTypes \
+    as ServerStates
+from cloudcafe.compute.servers_api.behaviors import ServerBehaviors
+from cloudcafe.common.tools.datagen import rand_name
+from cloudcafe.compute.common.exceptions import ItemNotFound, \
+    TimeoutException, BuildErrorException
+
+
+class ConfigDriveBehaviors(ServerBehaviors):
+
+    def get_openstack_metadata(self, server, servers_config, key):
+        """
+        @summary:Returns openstack metadata on config drive
+        @return: Response Object containing openstack meta domain object
+        @rtype: Request Response Object
+        """
+
+        remote_client = self.get_remote_instance_client(
+            server, servers_config, key=key)
+        openstack_meta_str = remote_client.get_file_details(
+            filepath='/mnt/config/openstack/latest/meta_data.json') 
+        return OpenStackMeta.deserialize(openstack_meta_str.content, 'json')
---
+++ cloudcafe/compute/extensions/config_drive/models/cd_openstack_meta.py
@@ -0,0 +1,92 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+import xml.etree.ElementTree as ET
+
+from cafe.engine.models.base import AutoMarshallingModel
+from cloudcafe.compute.common.models.link import Links
+from cloudcafe.compute.common.equality_tools import EqualityTools
+from cloudcafe.compute.common.constants import Constants
+from cloudcafe.compute.common.models.metadata import Metadata
+
+
+class OpenStackMeta(AutoMarshallingModel):
+
+    def __init__(self, availability_zone, hostname, launch_index, name, 
+                 uuid, files, public_keys, meta):
+        self.availability_zone = availability_zone
+        self.hostname = hostname
+        self.launch_index = launch_index
+        self.name = name
+        self.uuid = uuid
+        self.files = files
+        self.public_keys = public_keys
+        self.meta = meta
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+        open_meta = cls._dict_to_obj(json_dict)
+        return open_meta
+
+    @classmethod
+    def _dict_to_obj(cls, json_dict):
+        print json_dict
+        openstack_meta = OpenStackMeta(json_dict.get('availability_zone'),
+                      json_dict.get('hostname'),
+                      json_dict.get('launch_index'),
+                      json_dict.get('name'), json_dict.get('meta'),
+                      json_dict.get('public_keys'),
+                      json_dict.get('uuid'))
+        if 'files' in json_dict:
+            openstack_meta.files = FIle._json_to_obj(json_dict['files'])
+        return openstack_meta
+
+
+class File(AutoMarshallingModel):
+
+    def __init__(self, content_path, path):
+
+        super(CreateFlavor, self).__init__()
+        self.content_path = content_path
+        self.path = content_path
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        """
+        Returns an instance of a Files based on the json serialized_str
+        passed in.
+        """
+        json_dict = json.loads(serialized_str)
+
+        if 'file' in json_dict.keys():
+            file = cls._dict_to_obj(json_dict['file'])
+            return file
+
+        if 'files' in json_dict.keys():
+            files = []
+            for file_dict in json_dict['files']:
+                file = cls._dict_to_obj(file_dict)
+                files.append(file)
+            return flavors
+
+    @classmethod
+    def _dict_to_obj(cls, file_dict):
+        """Helper method to turn dictionary into Server instance."""
+        file = File(content_path=file_dict.get('content_path'),
+                    path=file_dict.get('path'))
+        return file
