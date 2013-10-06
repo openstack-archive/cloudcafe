12268a05af4aab7b8b8590fb7716af1c4f0bd5cd
Daryl Walleck <daryl.walleck@RACKSPACE.COM>
2013-10-06 02:28:51 -0500

    Adds basic XenServer client and models
    * Models for VM, VBD, and VDI
    * Basic client calls for VM, VBD, and VDI records
    
    Change-Id: I942f146e17fa82ee87dd00444d38d04fad095f2b

---
+++ cloudcafe/compute/hypervisors/xenserver/client.py
@@ -0,0 +1,70 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import XenAPI
+
+from cafe.engine.clients.base import BaseClient
+from cloudcafe.compute.hypervisors.xenserver.models.virtual_machine \
+    import VirtualMachine, VirtualBlockDevice, VirtualDiskImage
+
+
+class XenAPIClient(BaseClient):
+
+    def __init__(self, url, username, password):
+
+        self.session = XenAPI.Session(url)
+        self.session.xenapi.login_with_password(username, password)
+
+    def get_vm_record(self, server_id):
+        vm = self._get_vm_by_compute_id(server_id)
+        try:
+            record = self.session.xenapi.VM.get_record(vm)
+        except XenAPI.Failure as exception:
+                self._log.error(exception)
+                raise exception
+        virtual_machine = VirtualMachine._dict_to_obj(**record)
+        return virtual_machine
+
+    def get_vbd_record(self, vbd):
+        try:
+            record = self.session.xenapi.VBD.get_record(vbd)
+        except XenAPI.Failure as exception:
+                self._log.error(exception)
+                raise exception
+        block_device = VirtualBlockDevice._dict_to_obj(record)
+        return block_device
+
+    def get_vdi_record(self, vdi):
+        try:
+            record = self.session.xenapi.VDI.get_record(vdi)
+        except XenAPI.Failure as exception:
+                self._log.error(exception)
+                raise exception
+        vdi = VirtualDiskImage._dict_to_obj(record)
+        return vdi
+
+    def _get_vm_by_compute_id(self, server_id):
+        expected_vm_name = 'instance-{uuid}'.format(uuid=server_id)
+        try:
+            virtual_machines = self.session.xenapi.VM.get_all()
+        except XenAPI.Failure as exception:
+                self._log.error(exception)
+                raise exception
+        for vm in virtual_machines:
+            vm_name = self.session.xenapi.VM.get_name_label(vm)
+            if expected_vm_name == vm_name:
+                    return vm
+        return None
