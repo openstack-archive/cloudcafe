e120e982c6139bf0a5afd436bddef37521b1bbbe
Luke Wollney <luke.wollney@rackspace.com>
2013-11-08 11:11:02 -0600

    Refactor of image model
    *Refactored __init__ to account for correct properties
    *Removed datetime formating from __repr__
    *Removed true and false formatting from _json_to_obj
    *Refactored _dict_to_obj to account for not only correct properties, but also additional properties
    *Refactored _obj_to_json to account for correct properties
    *Fixed formating to be consistent
    *Added summary documentation for each class
    *Fixed checksum assignment
    *Removed direct_url
    *Added TODO tag to note the additional of direct_url and locations at a later date
    
    Change-Id: If53ebb21b336e4975239f2514ff8322972a847fb

--- cloudcafe/images/v2/models/image.py
+++ cloudcafe/images/v2/models/image.py
@@ -17,89 +17,59 @@ limitations under the License.
 import dateutil.parser
 import json
 
-from datetime import datetime
-
-from cafe.engine.models.base import AutoMarshallingModel, \
-    AutoMarshallingListModel
+from cafe.engine.models.base import \
+    AutoMarshallingListModel, AutoMarshallingModel
 from cloudcafe.compute.common.equality_tools import EqualityTools
 
 
 class Image(AutoMarshallingModel):
-    """@Summary Openstack Images(Glance) API 2.0 model"""
+    """@summary: Image v2 model"""
 
-    def __init__(self, id_=None, name=None, visibility=None,
-                 status=None, protected=None, tags=None, checksum=None,
-                 size=None, created_at=None, updated_at=None, file_=None,
-                 self_=None, schema=None, container_format=None,
-                 disk_format=None, min_disk=None, min_ram=None, kernel_id=None,
-                 ramdisk_id=None):
-        """@Summary Construct an Image model
-           @param visibility
-           @type ImageVisibility
-           @param status
-           @type ImageStatus
-           @param container_format
-           @type ImageContainerFormat
-           @param disk_format
-           @type ImageDiskFormat
-        """
+    def __init__(self, checksum=None, container_format=None, created_at=None,
+                 disk_format=None, file=None, id=None, min_disk=None,
+                 min_ram=None, name=None, protected=None, schema=None,
+                 self_=None, size=None, status=None, tags=None,
+                 updated_at=None, visibility=None, additional_properties=None):
 
-        self.id_ = id_
-        self.name = name
-        self.visibility = visibility
-        self.status = status
-        self.protected = protected
-        self.tags = tags
+        # TODO: 'direct_url' and 'locations' should be added at a later date
+
         self.checksum = checksum
-        self.size = size
-        self.created_at = created_at
-        self.updated_at = updated_at
-        self.file_ = file_
-        self.self_ = self_
-        self.schema = schema
         self.container_format = container_format
+        self.created_at = created_at
         self.disk_format = disk_format
+        self.file = file
+        self.id = id
         self.min_disk = min_disk
         self.min_ram = min_ram
-        self.kernel_id = kernel_id
-        self.ramdisk_id = ramdisk_id
+        self.name = name
+        self.protected = protected
+        self.schema = schema
+        self.self_ = self_
+        self.size = size
+        self.status = status
+        self.tags = tags
+        self.updated_at = updated_at
+        self.visibility = visibility
+        self.additional_properties = additional_properties
 
     def __eq__(self, other):
-        """
-        @summary: Overrides the default equals
-        @param other: Image object to compare with
-        @type other: Image
-        @return: True if Image objects are equal, False otherwise
-        @rtype: bool
-        """
+
         return EqualityTools.are_objects_equal(self, other)
 
     def __ne__(self, other):
-        """
-        @summary: Overrides the default not-equals
-        @param other: Image object to compare with
-        @type other: Image
-        @return: True if Image objects are not equal, False otherwise
-        @rtype: bool
-        """
+
         return not self.__eq__(other)
 
     def __repr__(self):
+
         values = []
         for prop in self.__dict__:
-            if prop in ['created_at', 'updated_at']:
-                date_property = self.__dict__[prop]
-                values.append("{0}: {1}".format(
-                    prop,
-                    date_property.strftime('%Y-%m-%dT%H:%M:%SZ')))
-            else:
-                values.append("{0}: {1}".format(prop, self.__dict__[prop]))
+            values.append("{0}: {1}".format(prop, self.__dict__[prop]))
         return '[{0}]'.format(', '.join(values))
 
     @classmethod
     def _json_to_obj(cls, serialized_str):
-        serialized_str.replace('false', 'False')
-        serialized_str.replace('true', 'True')
+
         json_dict = json.loads(serialized_str)
 
         if 'images' in json_dict.keys():
@@ -112,43 +82,55 @@ class Image(AutoMarshallingModel):
 
     @classmethod
     def _dict_to_obj(cls, json_dict):
-        """@summary: Process the json properties appropriately"""
 
-        for date_key in ['created_at', 'updated_at']:
-            if json_dict[date_key]:
-                json_dict[date_key] = datetime.strptime(json_dict[date_key],
-                                                        '%Y-%m-%dT%H:%M:%SZ')
-        for key in ['id', 'self', 'file']:
-            json_dict['{0}_'.format(key)] = json_dict[key]
-            del (json_dict[key])
+        additional_properties = {}
+        for key, value in json_dict.items():
+            if key not in ['checksum', 'container_format', 'created_at',
+                           'disk_format', 'file', 'id', 'min_disk', 'min_ram',
+                           'name', 'protected', 'schema', 'self', 'size',
+                           'status', 'tags', 'updated_at', 'visibility']:
+                additional_properties.update({key: value})
 
-        return Image(**json_dict)
+        image = Image(checksum=json_dict.get('checksum'),
+                      container_format=json_dict.get('container_format'),
+                      created_at=json_dict.get('created_at'),
+                      disk_format=json_dict.get('disk_format'),
+                      file=json_dict.get('file'), id=json_dict.get('id'),
+                      min_disk=json_dict.get('min_disk'),
+                      min_ram=json_dict.get('min_ram'),
+                      name=json_dict.get('name'),
+                      protected=json_dict.get('protected'),
+                      schema=json_dict.get('schema'),
+                      self_=json_dict.get('self'), size=json_dict.get('size'),
+                      status=json_dict.get('status'),
+                      tags=json_dict.get('tags'),
+                      updated_at=json_dict.get('updated_at'),
+                      visibility=json_dict.get('visibility'),
+                      additional_properties=additional_properties)
+
+        return image
 
     def _obj_to_json(self):
-        obj_dict = {}
-        if self.created_at:
-            obj_dict['created_at'] = \
-                self.created_at.strftime('%Y-%m-%dT%H:%M:%SZ')
-        if self.updated_at:
-            obj_dict['updated_at'] = \
-                self.updated_at.strftime('%Y-%m-%dT%H:%M:%SZ')
-        if self.self_:
-            obj_dict['self'] = self.self_
-        if self.file_:
-            obj_dict['file'] = self.file_
 
-        obj_dict['id'] = self.id_
-        obj_dict['name'] = self.name
-        obj_dict['visibility'] = self.visibility
-        obj_dict['status'] = self.status
-        obj_dict['protected'] = self.protected
-        obj_dict['tags'] = self.tags
+        obj_dict = {}
+
         obj_dict['checksum'] = self.checksum
-        obj_dict['size'] = self.size
         obj_dict['container_format'] = self.container_format
+        obj_dict['created_at'] = self.created_at
         obj_dict['disk_format'] = self.disk_format
+        obj_dict['file'] = self.file
+        obj_dict['id'] = self.id
         obj_dict['min_disk'] = self.min_disk
         obj_dict['min_ram'] = self.min_ram
+        obj_dict['name'] = self.name
+        obj_dict['protected'] = self.protected
+        obj_dict['schema'] = self.schema
+        obj_dict['self_'] = self.self_
+        obj_dict['size'] = self.size
+        obj_dict['status'] = self.status
+        obj_dict['tags'] = self.tags
+        obj_dict['updated_at'] = self.updated_at
+        obj_dict['visibility'] = self.visibility
 
         obj_dict = self._remove_empty_values(obj_dict)
 
@@ -156,29 +138,37 @@ class Image(AutoMarshallingModel):
 
     @classmethod
     def _xml_to_obj(cls, serialized_str):
+
         raise NotImplementedError("Glance does not serve XML-formatted \
                                   resources.")
 
     def _obj_to_xml(self):
+
         raise NotImplementedError("Glance does not serve XML-formatted \
                                   resources.")
 
 
 class Images(AutoMarshallingListModel):
-    """ An object that represents an images response object."""
+    """@summary: Images v2 model"""
 
     def __init__(self, images=None):
+
         super(Images, self).__init__()
+
         self.extend(images or [])
 
     @classmethod
     def _json_to_obj(cls, serialized_str):
+
         json_dict = json.loads(serialized_str)
+
         return cls._list_to_obj(json_dict.get('images'))
 
     @classmethod
     def _list_to_obj(cls, dict_list):
+
         images = Images()
+
         for image_dict in dict_list:
             images.append(Image._dict_to_obj(image_dict))
 
@@ -186,19 +176,18 @@ class Images(AutoMarshallingListModel):
 
 
 class ImagePatch(AutoMarshallingModel):
-    """
-        JSON model for updating an image.
-        http://docs.openstack.org/api/ \
-        openstack-image-service/2.0/content/update-an-image.html
-    """
+    """@summary: ImagePatch v2 model"""
 
     def __init__(self, add=None, replace=None, remove=None):
+
         self.add_dict = add
         self.replace_dict = replace
         self.remove_list = remove
 
     def _obj_to_json(self):
+
         replace_list = []
+
         if self.add_dict:
             for key, val in self.add_dict.items():
                 replace_list.append(
@@ -222,10 +211,11 @@ class ImagePatch(AutoMarshallingModel):
 
 
 class Member(AutoMarshallingModel):
-    """Image member model"""
+    """@summary: Member v2 model"""
 
     def __init__(self, member_id=None, status=None, created_at=None,
-                 updated_at=None, image_id=None):
+                  updated_at=None, image_id=None):
+
         self.member_id = member_id
         self.status = status
         self.created_at = created_at
@@ -233,23 +223,25 @@ class Member(AutoMarshallingModel):
         self.image_id = image_id
 
     def _obj_to_json(self):
-        json_dict = {"member": self.member_id,
-                     "status": self.status,
+
+        json_dict = {"member": self.member_id, "status": self.status,
                      "created_at": self.created_at,
-                     "updated_at": self.updated_at,
-                     "image_id": self.image_id}
+                     "updated_at": self.updated_at, "image_id": self.image_id}
 
         return json.dumps(json_dict)
 
     @classmethod
     def _json_to_obj(cls, serialized_str):
+
         json_dict = json.loads(serialized_str)
 
         return cls._dict_to_obj(json_dict)
 
     @classmethod
     def _dict_to_obj(cls, json_dict):
+
         del json_dict['schema']
+
         json_dict['created_at'] = dateutil.parser.parse(
             json_dict['created_at'])
 
@@ -260,13 +252,16 @@ class Member(AutoMarshallingModel):
 
 
 class Members(AutoMarshallingListModel):
-    """ An object that represents a members response object."""
+    """@summary: Members v2 model"""
 
     @classmethod
     def _json_to_obj(cls, serialized_str):
+
         json_dict = json.loads(serialized_str)
+
         return cls._list_to_obj(json_dict.get('members'))
 
     @classmethod
     def _list_to_obj(cls, dict_list):
+
         return [Member._dict_to_obj(member_dict) for member_dict in dict_list]
