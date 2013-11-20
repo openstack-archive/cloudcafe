a6070a3f0d215b6d324d547852b0595f5f151ecf
Luke Wollney <luke.wollney@rackspace.com>
2013-11-19 23:50:18 -0600

    Refactor images client for glance v2
    *Updated method names and other naming conventions to be more consistent and provide better context
    *Updated summary documentation and formating to be consistent throughout
    *Replaced id variables with id_
    *Replaced file variables with file_
    
    Change-Id: If205af7672ec1d9ba3448b9c37b0bba1d3996dab

--- cloudcafe/images/v2/client.py
+++ cloudcafe/images/v2/client.py
@@ -16,7 +16,7 @@ limitations under the License.
 
 from cafe.engine.clients.rest import AutoMarshallingRestClient
 from cloudcafe.images.v2.models.image import \
-    Image, Images, ImagePatch, Member, Members
+    Image, Images, ImageUpdate, Member, Members
 
 
 class ImagesClient(AutoMarshallingRestClient):
@@ -41,17 +41,17 @@ class ImagesClient(AutoMarshallingRestClient):
     def get_images_schema(self, requestslib_kwargs=None):
         """@summary: Get json schema of the images object"""
 
-        url = '{0}{1}'.format(self.base_url, '/schemas/images')
+        url = '{0}/{1}'.format(self.base_url, 'schemas/images')
         return self.request('GET', url, requestslib_kwargs=requestslib_kwargs)
 
     def get_image_schema(self, requestslib_kwargs=None):
         """@summary: Get json schema of the image object"""
 
-        url = '{0}{1}'.format(self.base_url, '/schemas/image')
+        url = '{0}/{1}'.format(self.base_url, 'schemas/image')
         return self.request('GET', url, requestslib_kwargs=requestslib_kwargs)
 
     def create_image(self, checksum=None, container_format=None,
-                     created_at=None, disk_format=None, file=None, id=None,
+                     created_at=None, disk_format=None, file_=None, id_=None,
                      min_disk=None, min_ram=None, name=None, protected=None,
                      schema=None, self_=None, size=None, status=None,
                      tags=None, updated_at=None, visibility=None,
@@ -60,7 +60,7 @@ class ImagesClient(AutoMarshallingRestClient):
 
         image = Image(checksum=checksum, container_format=container_format,
                       created_at=created_at, disk_format=disk_format,
-                      file=file, id=id, min_disk=min_disk, min_ram=min_ram,
+                      file_=file_, id_=id_, min_disk=min_disk, min_ram=min_ram,
                       name=name, protected=protected, schema=schema,
                       self_=self_, size=size, status=status, tags=tags,
                       updated_at=updated_at, visibility=visibility,
@@ -70,138 +70,135 @@ class ImagesClient(AutoMarshallingRestClient):
                             response_entity_type=Image,
                             requestslib_kwargs=requestslib_kwargs)
 
-    def get_image(self, image_id, requestslib_kwargs=None):
+    def get_image(self, id_, requestslib_kwargs=None):
         """@summary: Get a single image"""
 
-        url = '{0}/images/{1}'.format(self.base_url, image_id)
+        url = '{0}/images/{1}'.format(self.base_url, id_)
         return self.request('GET', url, response_entity_type=Image,
                             requestslib_kwargs=requestslib_kwargs)
 
-    def list_images(self, name=None, disk_format=None, container_format=None,
-                    visibility=None, status=None, checksum=None, owner=None,
-                    min_ram=None, min_disk=None, changes_since=None,
-                    protected=None, size_min=None, size_max=None,
-                    sort_key=None, sort_dir=None, marker=None, limit=None,
+    def list_images(self, changes_since=None, checksum=None,
+                    container_format=None, disk_format=None, limit=None,
+                    marker=None, min_disk=None, min_ram=None, name=None,
+                    owner=None, protected=None, size_max=None, size_min=None,
+                    sort_dir=None, sort_key=None, status=None, visibility=None,
                     requestslib_kwargs=None, **param_kwargs):
         """@summary: List all images"""
 
-        url = '{base_url}/images'.format(base_url=self.base_url)
-        params = {'name': name, 'disk_format': disk_format, 'status': status,
-                  'container_format': container_format, 'checksum': checksum,
-                  'visibility': visibility, 'owner': owner, 'min_ram': min_ram,
-                  'min_disk': min_disk, 'changes_since': changes_since,
-                  'protected': protected, 'size_min': size_min,
-                  'size_max': size_max, 'marker': marker, 'sort_key': sort_key,
-                  'sort_dir': sort_dir, 'limit': limit}
+        url = '{0}/images'.format(self.base_url)
+        params = {'changes_since': changes_since, 'checksum': checksum,
+                  'container_format': container_format,
+                  'disk_format': disk_format, 'limit': limit, 'marker': marker,
+                  'min_disk': min_disk, 'min_ram': min_ram, 'name': name,
+                  'owner': owner, 'protected': protected, 'size_max': size_max,
+                  'size_min': size_min, 'sort_dir': sort_dir,
+                  'sort_key': sort_key, 'status': status,
+                  'visibility': visibility}
         params.update(param_kwargs)
         return self.request('GET', url, params=params,
                             response_entity_type=Images,
                             requestslib_kwargs=requestslib_kwargs)
 
-    def update_image(self, image_id, replace=None, add=None, remove=None,
+    def update_image(self, id_, replace=None, add=None, remove=None,
                      requestslib_kwargs=None):
         """@summary: Update an existing image"""
 
-        url = '{0}/images/{1}'.format(self.base_url, image_id)
-        image_patch = ImagePatch(add, replace, remove)
+        url = '{0}/images/{1}'.format(self.base_url, id_)
+        image_update = ImageUpdate(add, replace, remove)
         headers = self.default_headers
         headers['Content-Type'] = (
             'application/openstack-images-v2.0-json-patch')
         return self.request('PATCH', url, headers=headers,
-                            request_entity=image_patch,
+                            request_entity=image_update,
                             response_entity_type=Image,
                             requestslib_kwargs=requestslib_kwargs)
 
-    def delete_image(self, image_id, requestslib_kwargs=None):
+    def delete_image(self, id_, requestslib_kwargs=None):
         """@summary: Delete an image"""
 
-        url = '{0}/images/{1}'.format(self.base_url, image_id)
+        url = '{0}/images/{1}'.format(self.base_url, id_)
         return self.request('DELETE', url,
                             requestslib_kwargs=requestslib_kwargs)
 
-    def store_raw_image_data(self, image_id, image_data,
-                             requestslib_kwargs=None):
-        """@summary: Upload image data"""
+    def store_image_file(self, id_, file_data, requestslib_kwargs=None):
+        """@summary: Store image file data on given image id"""
 
-        url = '{0}/images/{1}/file'.format(self.base_url, image_id)
+        url = '{0}/images/{1}/file'.format(self.base_url, id_)
         headers = {'Content-Type': 'application/octet-stream'}
-        return self.request('PUT', url, headers=headers, data=image_data)
+        return self.request('PUT', url, headers=headers, data=file_data)
 
-    def get_raw_image_data(self, image_id, requestslib_kwargs=None):
-        """@summary: Get image data"""
+    def get_image_file(self, id_, requestslib_kwargs=None):
+        """@summary: Get image file data for given image id"""
 
-        raise NotImplementedError
+        url = '{0}/images/{1}/file'.format(self.base_url, id_)
+        return self.request('GET', url, response_entity_type=Image,
+                            requestslib_kwargs=requestslib_kwargs)
 
-    def add_tag(self, image_id, tag, requestslib_kwargs=None):
+    def add_tag(self, id_, tag, requestslib_kwargs=None):
         """@summary: Add a tag to an image"""
 
-        url = '{base_url}/images/{image_id}/tags/{tag}'.format(
-            base_url=self.base_url, image_id=image_id, tag=tag)
+        url = '{0}/images/{1}/tags/{2}'.format(self.base_url, id_, tag)
         return self.request('PUT', url, requestslib_kwargs=requestslib_kwargs)
 
-    def delete_tag(self, image_id, tag, requestslib_kwargs=None):
+    def delete_tag(self, id_, tag, requestslib_kwargs=None):
         """@summary: Delete a tag from an image"""
 
-        url = '{base_url}/images/{image_id}/tags/{tag}'.format(
-            base_url=self.base_url, image_id=image_id, tag=tag)
+        url = '{0}/images/{1}/tags/{2}'.format(self.base_url, id_, tag)
         return self.request('DELETE', url,
                             requestslib_kwargs=requestslib_kwargs)
 
     def get_image_members_schema(self, requestslib_kwargs=None):
         """@summary: Get schema of the image members object"""
 
-        url = '{0}{1}'.format(self.base_url, '/schemas/members')
+        url = '{0}/{1}'.format(self.base_url, 'schemas/members')
         return self.request('GET', url, requestslib_kwargs=requestslib_kwargs)
 
     def get_image_member_schema(self, requestslib_kwargs=None):
         """@summary: Get schema of the image member object"""
 
-        url = '{0}{1}'.format(self.base_url, '/schemas/member')
+        url = '{0}/{1}'.format(self.base_url, 'schemas/member')
         return self.request('GET', url, requestslib_kwargs=requestslib_kwargs)
 
-    def add_member(self, image_id, member_id, requestslib_kwargs=None):
+    def add_member(self, id_, member_id, requestslib_kwargs=None):
         """@summary: Add a member to an image"""
 
-        url = '{base_url}/images/{image_id}/members'.format(
-            base_url=self.base_url, image_id=image_id)
-        member = Member(image_id=image_id, member_id=member_id)
+        url = '{0}/images/{1}/members'.format(self.base_url, id_)
+        member = Member(member_id=member_id)
         return self.request('POST', url, request_entity=member,
                             response_entity_type=Member,
                             requestslib_kwargs=requestslib_kwargs)
 
-    def list_members(self, image_id):
+    def list_members(self, id_):
         """@summary: List all image members"""
 
-        url = '{base_url}/images/{image_id}/members'.format(
-            base_url=self.base_url, image_id=image_id)
+        url = '{0}/images/{1}/members'.format(self.base_url, id_)
         return self.request('GET', url, response_entity_type=Members)
 
-    def update_member(self, image_id, member_id, status,
-                        requestslib_kwargs=None):
+    def update_member(self, id_, member_id, status, requestslib_kwargs=None):
         """@summary: Update a member for an image"""
 
-        url = '{base_url}/images/{image_id}/members/{member_id}'.format(
-            base_url=self.base_url, image_id=image_id, member_id=member_id)
-        member = Member(image_id=image_id, member_id=member_id, status=status)
+        url = '{0}/images/{1}/members/{2}'.format(self.base_url, id_,
+                                                  member_id)
+        member = Member(status=status)
         return self.request('PUT', url, request_entity=member,
                             response_entity_type=Member,
                             requestslib_kwargs=requestslib_kwargs)
 
-    def delete_member(self, image_id, member_id):
+    def delete_member(self, id_, member_id):
         """@summary: Delete a member from an image"""
 
-        url = '{base_url}/images/{image_id}/members/{member_id}'.format(
-            base_url=self.base_url, image_id=image_id, member_id=member_id)
+        url = '{0}/images/{1}/members/{2}'.format(self.base_url, id_,
+                                                  member_id)
         return self.request('DELETE', url)
 
     def get_tasks_schema(self, requestslib_kwargs=None):
         """@summary: Get schema of the tasks object"""
 
-        url = '{0}{1}'.format(self.base_url, '/schemas/tasks')
+        url = '{0}/{1}'.format(self.base_url, 'schemas/tasks')
         return self.request('GET', url, requestslib_kwargs=requestslib_kwargs)
 
     def get_task_schema(self, requestslib_kwargs=None):
         """@summary: Get schema of the task object"""
 
-        url = '{0}{1}'.format(self.base_url, '/schemas/task')
+        url = '{0}/{1}'.format(self.base_url, 'schemas/task')
         return self.request('GET', url, requestslib_kwargs=requestslib_kwargs)
