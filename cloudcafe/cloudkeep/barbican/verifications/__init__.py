0367cf5516a9c5bfb2a5af801c06fb691066e0e8
Steve Heyman <steve.heyman@rackspace.com>
2013-12-08 18:19:25 -0600

    Implements: scrumdo story 122: added smoke tests for verifications
    Change-Id: If5ae0edaba6d2a96c92e38a708094878beec4258

--- cloudcafe/cloudkeep/barbican/verifications/__init__.py
+++ cloudcafe/cloudkeep/barbican/verifications/__init__.py
@@ -13,16 +13,3 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-
-class SecretsStates(object):
-    ACTIVE = "ACTIVE"
-    ERROR = "ERROR"
-
-
-class OrdersStates(SecretsStates):
-    PENDING = "PENDING"
-
-
-class VerificationsStates(SecretsStates):
-    PENDING = "PENDING"
