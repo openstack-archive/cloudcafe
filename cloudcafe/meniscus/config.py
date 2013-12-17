904f36e581653103c01d24caed5b747401994032
John Vrbanac <john.vrbanac@rackspace.com>
2013-12-16 20:57:44 -0600

    Updating meniscus supporting classes after more product changes
    Change-Id: Ic08deed1eb0cab879f4c7d0a624a676bd18f7420

--- cloudcafe/meniscus/config.py
+++ cloudcafe/meniscus/config.py
@@ -101,66 +101,6 @@ class TenantConfig(ConfigSectionInterface):
         return self.get("profile_name")
 
 
-class PairingConfig(ConfigSectionInterface):
-    SECTION_NAME = 'meniscus-pairing'
-
-    @property
-    def hostname(self):
-        return self.get('hostname')
-
-    @property
-    def coordinator_base_url(self):
-        return self.get('coordinator_base_url')
-
-    @property
-    def worker_base_url(self):
-        return self.get('worker_base_url')
-
-    @property
-    def callback(self):
-        return self.get('callback')
-
-    @property
-    def ip_v4(self):
-        return self.get('ip_address_v4')
-
-    @property
-    def ip_v6(self):
-        return self.get('ip_address_v6')
-
-    @property
-    def os_type(self):
-        return self.get('os_type')
-
-    @property
-    def memory_mb(self):
-        return int(self.get('memory_mb'))
-
-    @property
-    def arch(self):
-        return self.get('arch')
-
-    @property
-    def api_secret(self):
-        return self.get('api_secret')
-
-    @property
-    def personality(self):
-        return self.get('personality')
-
-    @property
-    def cpu_cores(self):
-        return int(self.get('cpu_cores'))
-
-    @property
-    def load_average(self):
-        return json_to_dict(self.get('load_average'))
-
-    @property
-    def disks(self):
-        return json_to_dict(self.get('disks'))
-
-
 class CorrelationConfig(ConfigSectionInterface):
     SECTION_NAME = 'meniscus-correlation'
 
