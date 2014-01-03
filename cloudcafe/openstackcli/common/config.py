c8ef7ce18051a7100039a43ca71871e4183b2eee
Jose Idar <jose.idar@rackspace.com>
2014-01-03 11:11:16 -0600

    CinderCLI test infrastructure buildout
    * Adds openstackcli common config section
    * Adds cindercli common config section
    * Adds show_volume() and create_snapshot() to cinder cli
    * Adds cinder cli behaviors:
      create_available_volume()
      get_volume_status()
      wait_for_volume_status()
      list_volumes()
      delete_volume()
      wait_for_volume_delete()
      list_snapshots()
      get_snapshot_status()
      list_volume_snapshot_ids()
      delete_volume_snapshots()
      wait_for_snapshot_status()
      wait_for_snapshot_delete()
      list_volume_types()
    * Moves relevant behavior defaults from method signature
      to config.
    * Fixes snapshot response model to include progress and
      project_id attributes.
    
    Change-Id: I8367be36d549ab67ec634f46042d47efa00547c0

---
+++ cloudcafe/openstackcli/common/config.py
@@ -0,0 +1,56 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cloudcafe.common.models.configuration import ConfigSectionInterface
+
+
+class OpenstackCLI_CommonConfig(ConfigSectionInterface):
+
+    SECTION_NAME = 'openstack_cli_common'
+
+    @property
+    def debug(self):
+        """Running the openstack cli with debug on by default gives much more
+           useful output in the logs"""
+        return self.get_boolean('debug', True)
+
+    @property
+    def retries(self):
+        return self.get('retries', None)
+
+    @property
+    def os_username(self):
+        return self.get('os_username')
+
+    @property
+    def os_password(self):
+        return self.get('os_password')
+
+    @property
+    def os_tenant_name(self):
+        return self.get('os_tenant_name')
+
+    @property
+    def os_auth_url(self):
+        return self.get('os_auth_url')
+
+    @property
+    def os_region_name(self):
+        return self.get('os_region_name')
+
+    @property
+    def os_cacert(self):
+        return self.get('os_cacert')
