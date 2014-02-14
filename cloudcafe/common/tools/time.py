8d461af68b43e7b1375b5c0d4c5e0ed1d930744e
Priyanka Agrawal <priyanka.agrawal@RACKSPACE.COM>
2014-02-14 17:31:19 +0530

    Added are_datetimestrings_equal method
    Added are_datetimestrings_equal method for comparison
    Added docstrings for string_to_datetime method
    
    Change-Id: Ia4bcebe8f7a6f3b9663bb59a13f79d8a61ccf5b0

--- cloudcafe/common/tools/time.py
+++ cloudcafe/common/tools/time.py
@@ -15,6 +15,8 @@ limitations under the License.
 """
 from datetime import datetime, timedelta
 
+from cloudcafe.compute.common.equality_tools import EqualityTools
+
 
 def get_tomorrow_timestamp():
     tomorrow = (datetime.today() + timedelta(days=1))
@@ -22,6 +24,15 @@ def get_tomorrow_timestamp():
 
 
 def string_to_datetime(datetimestring, date_formats=None):
+    """
+    @summary: This method converts a string to a datetime in a given format
+            or according to some predefined format.
+    @param datetimestring: string to be converted into date
+    @type datetimestring: string
+    @param date_formats: The date format in which string needs
+                        to be converted into
+    @type date_formats: datetime
+    """
     date_formats = date_formats or [
         '%Y-%m-%d %H:%M:%S', '%Y-%m-%d %H:%M:%S.%f', '%Y-%m-%dT%H:%M:%S.%fZ',
         '%Y-%m-%dT%H:%M:%S.%f', "%Y-%m-%dT%H:%M:%SZ", "%Y-%m-%dT%H:%M:%S"]
@@ -33,3 +44,20 @@ def string_to_datetime(datetimestring, date_formats=None):
             continue
     else:
         raise
+
+
+def are_datetimestrings_equal(datetimestring1, datetimestring2, leeway=0):
+    """
+    @summary: This method compares two datetime strings with some
+            permissible leeway.
+    @param datetimestring1: datetime string to be compared
+    @type datetimestring1: string
+    @param datetimestring2: datetime string to be compared with
+    @type datetimestring2: string
+    @param leeway: permissible difference in equality
+    @type leeway: int
+    """
+    return EqualityTools.are_datetimes_equal(
+        string_to_datetime(datetimestring1),
+        string_to_datetime(datetimestring2),
+        timedelta(seconds=int(leeway)))
