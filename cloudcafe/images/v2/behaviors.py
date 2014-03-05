21918b3826f2e6b3f9099fc4c1c21446b37dd0a2
Luke Wollney <luke.wollney@RACKSPACE.COM>
2014-03-05 08:52:01 -0600

    Update images behaviors
    *Updated tasks related methods
    *Updated create image method names for better clarity
    *Corrected missed create image call
    *Removed passing  client to wait_for_task_status
    *Updated all None checks to use shorthand notation
    *Updated white space to be more consistent
    *Updated validate_task validation efficiency
    
    Change-Id: I1e1f986cfc86a6eeb8d7b93b0df306fee6e58e6b

--- cloudcafe/images/v2/behaviors.py
+++ cloudcafe/images/v2/behaviors.py
@@ -14,15 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import re
 import calendar
+import re
 import time
 
 from cafe.engine.behaviors import BaseBehavior
-from cloudcafe.common.exceptions import BuildErrorException, TimeoutException
 from cloudcafe.common.resources import ResourcePool
 from cloudcafe.common.tools.datagen import rand_name
 from cloudcafe.images.common.constants import ImageProperties, Messages
+from cloudcafe.images.common.exceptions import (
+    BuildErrorException, RequiredResourceException, TimeoutException)
 from cloudcafe.images.common.types import (
     ImageContainerFormat, ImageDiskFormat, ImageStatus, Schemas, TaskStatus,
     TaskTypes)
@@ -39,38 +40,78 @@ class ImagesBehaviors(BaseBehavior):
         self.error_msg = Messages.ERROR_MSG
         self.id_regex = re.compile(ImageProperties.ID_REGEX)
 
+    def create_image_via_task(self, image_properties=None, import_from=None,
+                              import_from_format=None):
+        """
+        @summary: Create new image via the create new task method and add it
+        for deletion
+        """
+
+        image_properties = image_properties or {'name': rand_name('image')}
+        import_from = import_from or self.config.import_from
+        import_from_format = import_from or self.config.import_from_format
+
+        input_ = {'image_properties': image_properties,
+                  'import_from': import_from,
+                  'import_from_format': import_from_format}
+        task = self.create_new_task(input_=input_, type_=TaskTypes.IMPORT)
+        image_id = task.result.image_id
+
+        response = self.client.get_image(image_id=image_id)
+        image = response.entity
+
+        if image is not None:
+            self.resources.add(image.id_, self.client.delete_image)
+
+        return image
+
+    def create_images_via_task(self, image_properties=None, import_from=None,
+                               import_from_format=None, count=1):
+        """
+        @summary: Create new images via the create new task method and add them
+        for deletion
+        """
+
+        image_list = []
+
+        for i in range(count):
+            image = self.create_new_image(
+                image_properties=image_properties, import_from=import_from,
+                import_from_format=import_from_format)
+            image_list.append(image)
+
+        return image_list
+
     def create_new_image(self, container_format=None, disk_format=None,
-                         name=None, protected=None, tags=None,
-                         visibility=None):
+                         name=None, protected=None, tags=None):
         """@summary: Create new image and add it for deletion"""
 
-        if container_format is None:
-            container_format = ImageContainerFormat.BARE
-        if disk_format is None:
-            disk_format = ImageDiskFormat.RAW
-        if name is None:
-            name = rand_name('image')
+        container_format = container_format or ImageContainerFormat.BARE
+        disk_format = disk_format or ImageDiskFormat.RAW
+        name = name or rand_name('image')
 
         response = self.client.create_image(
             container_format=container_format, disk_format=disk_format,
-            name=name, protected=protected, tags=tags, visibility=visibility)
+            name=name, protected=protected, tags=tags)
         image = response.entity
+
         if image is not None:
             self.resources.add(image.id_, self.client.delete_image)
+
         return image
 
     def create_new_images(self, container_format=None, disk_format=None,
-                          name=None, protected=None, tags=None,
-                          visibility=None, count=1):
+                          name=None, protected=None, tags=None, count=1):
         """@summary: Create new images and add them for deletion"""
 
         image_list = []
+
         for i in range(count):
             image = self.create_new_image(
                 container_format=container_format, disk_format=disk_format,
-                name=name, protected=protected, tags=tags,
-                visibility=visibility)
+                name=name, protected=protected, tags=tags)
             image_list.append(image)
+
         return image_list
 
     def list_images_pagination(self, changes_since=None, checksum=None,
@@ -84,6 +125,7 @@ class ImagesBehaviors(BaseBehavior):
 
         image_list = []
         results_limit = self.config.results_limit
+
         response = self.client.list_images(
             changes_since=changes_since, checksum=checksum,
             container_format=container_format, disk_format=disk_format,
@@ -93,6 +135,7 @@ class ImagesBehaviors(BaseBehavior):
             sort_dir=sort_dir, sort_key=sort_key, status=status,
             visibility=visibility)
         images = response.entity
+
         while len(images) == results_limit:
             image_list += images
             marker = images[results_limit - 1].id_
@@ -105,7 +148,9 @@ class ImagesBehaviors(BaseBehavior):
                 sort_dir=sort_dir, sort_key=sort_key, status=status,
                 visibility=visibility)
             images = response.entity
+
         image_list += images
+
         return image_list
 
     def get_member_ids(self, image_id):
@@ -134,19 +179,34 @@ class ImagesBehaviors(BaseBehavior):
         """
 
         errors = []
+
+        if image.auto_disk_config is None:
+            errors.append(self.error_msg.format(
+                'auto_disk_config', 'not None', image.auto_disk_config))
         if image.created_at is None:
-            errors.append(self.error_msg.format('created_at', not None, None))
+            errors.append(self.error_msg.format(
+                'created_at', 'not None', image.created_at))
         if image.file_ != '/v2/images/{0}/file'.format(image.id_):
             errors.append(self.error_msg.format(
                 'file_', '/v2/images/{0}/file'.format(image.id_), image.file_))
+        if image.image_type is None:
+            errors.append(self.error_msg.format(
+                'image_type', 'not None', image.image_type))
         if self.id_regex.match(image.id_) is None:
-            errors.append(self.error_msg.format('id_', not None, None))
+            errors.append(self.error_msg.format(
+                'id_', 'not None', self.id_regex))
         if image.min_disk is None:
-            errors.append(self.error_msg.format('min_disk', not None, None))
+            errors.append(self.error_msg.format(
+                'min_disk', 'not None', image.min_disk))
         if image.min_ram is None:
-            errors.append(self.error_msg.format('min_ram', not None, None))
+            errors.append(self.error_msg.format(
+                'min_ram', 'not None', image.min_ram))
+        if image.os_type is None:
+            errors.append(self.error_msg.format(
+                'os_type', 'not None', image.os_type))
         if image.protected is None:
-            errors.append(self.error_msg.format('protected', not None, None))
+            errors.append(self.error_msg.format(
+                'protected', 'not None', image.protected))
         if image.schema != Schemas.IMAGE_SCHEMA:
             errors.append(self.error_msg.format(
                 'schema', Schemas.IMAGE_SCHEMA, image.schema))
@@ -154,9 +214,15 @@ class ImagesBehaviors(BaseBehavior):
             errors.append(self.error_msg.format(
                 'schema', '/v2/images/{0}'.format(image.id_), image.self_))
         if image.status is None:
-            errors.append(self.error_msg.format('status', not None, None))
+            errors.append(self.error_msg.format(
+                'status', 'not None', image.status))
         if image.updated_at is None:
-            errors.append(self.error_msg.format('updated_at', not None, None))
+            errors.append(self.error_msg.format(
+                'updated_at', 'not None', image.updated_at))
+        if image.user_id is None:
+            errors.append(self.error_msg.format(
+                'user_id', 'not None', image.user_id))
+
         return errors
 
     def validate_image_member(self, image_id, image_member, member_id):
@@ -165,8 +231,10 @@ class ImagesBehaviors(BaseBehavior):
         """
 
         errors = []
+
         if image_member.created_at is None:
-            errors.append(self.error_msg.format('created_at', not None, None))
+            errors.append(self.error_msg.format(
+                'created_at', 'not None', image_member.created_at))
         if image_member.image_id != image_id:
             errors.append(self.error_msg.format(
                 'image_id', image_id, image_member.image_id))
@@ -177,9 +245,12 @@ class ImagesBehaviors(BaseBehavior):
             errors.append(self.error_msg.format(
                 'schema', Schemas.IMAGE_MEMBER_SCHEMA, image_member.schema))
         if image_member.status is None:
-            errors.append(self.error_msg.format('status', not None, None))
+            errors.append(self.error_msg.format(
+                'status', 'not None', image_member.status))
         if image_member.updated_at is None:
-            errors.append(self.error_msg.format('updated_at', not None, None))
+            errors.append(self.error_msg.format(
+                'updated_at', 'not None', image_member.updated_at))
+
         return errors
 
     def wait_for_image_status(self, image_id, desired_status,
@@ -228,20 +299,28 @@ class ImagesBehaviors(BaseBehavior):
     def create_new_task(self, input_=None, type_=None):
         """@summary: Create new task and wait for success status"""
 
-        if input_ is None:
-            input_ = {'image_properties': {},
-                      'import_from': self.config.import_from,
-                      'import_from_format': self.config.import_from_format}
-        if type_ is None:
-            type_ = TaskTypes.IMPORT
+        import_from = self.config.import_from
+        import_from_format = self.config.import_from_format
+        input_ = input_ or {'image_properties': {},
+                            'import_from': import_from,
+                            'import_from_format': import_from_format}
+        type_ = type_ or TaskTypes.IMPORT
+        failures = []
+        attempts = self.config.resource_creation_attempts
 
-        response = self.client.create_task(input_=input_, type_=type_)
-
-        task_id = response.entity.id_
-
-        task = self.wait_for_task_status(task_id, TaskStatus.SUCCESS)
-
-        return task
+        for attempt in range(attempts):
+            try:
+                response = self.client.create_task(input_=input_, type_=type_)
+                task_id = response.entity.id_
+                task = self.wait_for_task_status(task_id, TaskStatus.SUCCESS)
+                return task
+            except (TimeoutException, BuildErrorException) as ex:
+                self._log.error('Failed to create task with uuid {0}: '
+                                '{1}'.format(task_id, ex.message))
+                failures.append(ex.message)
+        raise RequiredResourceException(
+            'Failed to successfully create a task after {0} attempts: '
+            '{1}'.format(attempts, failures))
 
     def create_new_tasks(self, input_=None, type_=None, count=1):
         """@summary: Create new tasks and wait for success status for each"""
@@ -264,7 +343,6 @@ class ImagesBehaviors(BaseBehavior):
         response = self.client.list_tasks(
             limit=limit, marker=marker, sort_dir=sort_dir, status=status,
             type_=type_)
-
         tasks = response.entity
 
         while len(tasks) == results_limit:
@@ -288,61 +366,82 @@ class ImagesBehaviors(BaseBehavior):
 
         if task.status is None:
             errors.append(self.error_msg.format(
-                'status', not None, task.status))
+                'status', 'not None', task.status))
         if self.id_regex.match(task.id_) is None:
             errors.append(self.error_msg.format(
-                'id_', not None, self.id_regex.match(task.id_)))
+                'id_', 'not None', self.id_regex.match(task.id_)))
         if task.created_at is None:
             errors.append(self.error_msg.format(
-                'created_at', not None, task.created_at))
-        if (task.status == TaskStatus.PENDING or
-                task.status == TaskStatus.PROCESSING and
-                task.input_.image_properties != {}):
+                'created_at', 'not None', task.created_at))
+        if task.type_ == TaskTypes.IMPORT:
+            if task.input_.import_from is None:
+                errors.append(self.error_msg.format(
+                    'import_from', 'not None', task.input_.import_from))
+            if task.input_.import_from_format is None:
+                errors.append(self.error_msg.format(
+                    'import_from_format', 'not None',
+                    task.input_.import_from_format))
+            if (task.result is not None and
+                    self.id_regex.match(task.result.image_id) is None):
+                errors.append(self.error_msg.format(
+                    'image_id', 'not None',
+                    self.id_regex.match(task.result.image_id)))
+        elif task.type_ == TaskTypes.EXPORT:
+            if task.input_.image_uuid is None:
+                errors.append(self.error_msg.format(
+                    'image_uuid', 'not None', task.input_.image_uuid))
+            if task.input_.receiving_swift_container is None:
+                errors.append(self.error_msg.format(
+                    'receiving_swift_container', 'not None',
+                    task.input_.receiving_swift_container))
+            if task.result is not None and task.result.export_location is None:
+                errors.append(self.error_msg.format(
+                    'export_location', 'not None',
+                    task.result.export_location))
+        elif task.type_ is None:
             errors.append(self.error_msg.format(
-                'image_properties', not {}, task.input_.image_properties))
-        elif (task.status != TaskStatus.PENDING or
-                task.status != TaskStatus.PROCESSING and
-                task.input_.image_properties == {}):
-            errors.append(self.error_msg.format(
-                'image_properties', not {}, task.input_.image_properties))
-        if task.input_.import_from is None:
-            errors.append(self.error_msg.format(
-                'import_from', not None, task.input_.import_from))
-        if task.input_.import_from_format is None:
-            errors.append(self.error_msg.format(
-                'import_from_format', not None,
-                task.input_.import_from_format))
-        if task.expires_at is not None:
-            errors.append(self.error_msg.format(
-                'expires_at', None, task.expires_at))
+                'type_', 'not None', task.type_))
         if task.updated_at is None:
             errors.append(self.error_msg.format(
-                'updated_at', not None, task.updated_at))
+                'updated_at', 'not None', task.updated_at))
         if task.self_ != '/v2/tasks/{0}'.format(task.id_):
             errors.append(self.error_msg.format(
                 'self_', '/v2/tasks/{0}'.format(task.id_), task.self_))
-        if task.type_ is None:
-            errors.append(self.error_msg.format(
-                'type_', not None, task.type_))
-        if (task.result is not None and
-                self.id_regex.match(task.result.image_id) is None):
-            errors.append(self.error_msg.format(
-                'image_id', not None,
-                self.id_regex.match(task.result.image_id)))
         if task.owner is None:
             errors.append(self.error_msg.format(
-                'owner', not None, task.owner))
-        if task.message is not None:
+                'owner', 'not None', task.owner))
+        if task.message != 'None':
             errors.append(self.error_msg.format(
-                'message', None, task.message))
+                'message', 'None', task.message))
         if task.schema != '/v2/schemas/task':
             errors.append(self.error_msg.format(
                 'schema', '/v2/schemas/task', task.schema))
 
         return errors
 
-    def wait_for_task_status(self, task_id, desired_status,
-                             interval_time=None, timeout=None):
+    def validate_exported_files(self, export_to, expect_success, files,
+                                image_id):
+        """
+        @summary: Validate that a given cloud files location contains a
+        given file or not
+        """
+
+        errors = []
+        file_names = [file_.name for file_ in files]
+
+        if expect_success:
+            if '{0}.vhd'.format(image_id) not in file_names:
+                errors.append(self.error_msg.format(
+                    'file present', True, False))
+        else:
+            if '{0}.vhd'.format(image_id) in file_names:
+                errors.append(self.error_msg.format(
+                    'file present', False, True))
+
+        return errors, file_names
+
+    def wait_for_task_status(self, task_id, desired_status, interval_time=None,
+                             timeout=None):
         """@summary: Waits for a task to reach a desired status"""
 
         interval_time = interval_time or self.config.task_status_interval
@@ -353,19 +452,25 @@ class ImagesBehaviors(BaseBehavior):
             resp = self.client.get_task(task_id)
             task = resp.entity
 
-            if task.status.lower() == TaskStatus.FAILURE.lower():
+            if ((task.status.lower() == TaskStatus.FAILURE and
+                    desired_status != TaskStatus.FAILURE) or
+                    (task.status.lower() == TaskStatus.SUCCESS and
+                     desired_status != TaskStatus.SUCCESS)):
                 raise BuildErrorException(
-                    'Task with uuid {0} entered FAILURE status.'
-                    'Task responded with the message {1}'.format(task.id_,
-                                                                 task.message))
+                    'Task with uuid {0} entered {1} status. Task responded '
+                    'with the message {2}'.format(
+                        task.id_, task.status, task.message))
 
             if task.status == desired_status:
                 break
             time.sleep(interval_time)
         else:
             raise TimeoutException(
-                "wait_for_task_status ran for {0} seconds and did not "
-                "observe task {1} reach the {2} status.".format(
-                    timeout, task_id, desired_status))
+                'Failed to reach the {0} status after {1} seconds for task '
+                'with uuid {2}'.format(desired_status, timeout, task_id))
+
+        if (task is not None and task.type_ == TaskTypes.IMPORT and
+                task.status.lower() == TaskStatus.SUCCESS):
+            self.resources.add(task.result.image_id, self.client.delete_image)
 
         return task
