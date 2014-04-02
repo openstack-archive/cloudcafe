d9fe1c6fa51c1a3e72963f687775bd1f48b3840b
ivo5307 <ivo.vasev@RACKSPACE.COM>
2014-04-02 15:11:18 -0500

    Boot from Block version 2 support
        * Clinet for create V2 boot from block server
        * Models for V2 boot from block and block device V2
        * Unittestst added for models
        * Configuration changes boot from volume V1
        * Fixed review comments
        * Removed Inherited Boot from volume config
        * Added Block Device behaviours
        * Typo fixed
    
    Change-Id: I4948e70483c12ea0b0c3509b03adb765375b0717

---
+++ cloudcafe/compute/extensions/extensions_api/clients/client.py
@@ -0,0 +1,117 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from cafe.engine.http.client import AutoMarshallingHTTPClient
+
+from cloudcafe.compute.extensions.extensions_api.models.request import \
+    CreateServerFromVolume as CreateServer
+from cloudcafe.compute.servers_api.models.servers import Server
+
+
+class VolumeServersClient(AutoMarshallingHTTPClient):
+
+    def __init__(self, url, auth_token, serialize_format=None,
+                 deserialize_format=None):
+        """
+        @param url: Base URL for the compute service
+        @type url: String
+        @param auth_token: Auth token to be used for all requests
+        @type auth_token: String
+        @param serialize_format: Format for serializing requests
+        @type serialize_format: String
+        @param deserialize_format: Format for de-serializing responses
+        @type deserialize_format: String
+        """
+        super(VolumeServersClient, self).__init__(serialize_format,
+                                                  deserialize_format)
+        self.auth_token = auth_token
+        self.default_headers['X-Auth-Token'] = auth_token
+        ct = '{content_type}/{content_subtype}'.format(
+            content_type='application',
+            content_subtype=self.serialize_format)
+        accept = '{content_type}/{content_subtype}'.format(
+            content_type='application',
+            content_subtype=self.deserialize_format)
+        self.default_headers['Content-Type'] = ct
+        self.default_headers['Accept'] = accept
+        self.url = url
+
+    def create_server(self, name, flavor_ref, block_device_mapping_v2,
+                      max_count=None, min_count=None, networks=None,
+                      image_ref=None, personality=None, user_data=None,
+                      metadata=None, accessIPv4=None, accessIPv6=None,
+                      disk_config=None, admin_pass=None, key_name=None,
+                      config_drive=None, scheduler_hints=None,
+                      requestslib_kwargs=None):
+        """
+        @summary: Creates an instance of a  block Version 2 server given the
+         provided parameters
+        @param name: Name of the server
+        @type name: String
+        @param flavor_ref: Identifier for the flavor used to build the server
+        @type flavor_ref: String
+        @param block_device_mapping_v2: A list of dictionaries needed for boot
+         from volume V2 feature
+        @type block_device_mapping: List
+        @param max_count: max_count parameter for the server.
+        @type max_count: String
+        @param min_count: min_count parameter for the server.
+        @type min_count: String
+        @param networks: Networks for the server.
+        @type networks: List
+        @param image_ref: Will be used for negative testing
+        @type image_ref: String
+        @param personality: A list of dictionaries for files to be
+         injected into the server.
+        @type personality: List
+        @param user_data: Config Init User data
+        @type user_data: String
+        @param metadata: A dictionary of values to be used as server metadata
+        @type meta: Dictionary
+        @param accessIPv4: IPv4 address for the server.
+        @type accessIPv4: String
+        @param accessIPv6: IPv6 address for the server.
+        @type accessIPv6: String
+        @param disk_config: MANUAL/AUTO/None
+        @type disk_config: String
+        @param admin_pass: Injected Pass
+        @type admin_pass: String
+        @param key_name: Key name
+        @type key_name: String
+        @param config_drive: false/true
+        @type config_drive: String
+        @param scheduler_hints: Target compute
+        @type scheduler_hints: String
+        @return: Response Object containing response code and
+         the server domain object
+        @rtype: Requests.response
+        """
+
+        server_request_object = CreateServer(
+            name=name, flavor_ref=flavor_ref,
+            block_device_mapping_v2=block_device_mapping_v2,
+            max_count=max_count, min_count=min_count, networks=networks,
+            image_ref=image_ref, personality=personality, user_data=user_data,
+            metadata=metadata, accessIPv4=accessIPv4, accessIPv6=accessIPv6,
+            disk_config=disk_config, admin_pass=admin_pass, key_name=key_name,
+            config_drive=config_drive, scheduler_hints=scheduler_hints)
+
+        url = '{base_url}/os-volumes_boot'.format(base_url=self.url)
+        resp = self.request('POST', url,
+                            response_entity_type=Server,
+                            request_entity=server_request_object,
+                            requestslib_kwargs=requestslib_kwargs)
+        return resp
---
+++ cloudcafe/compute/extensions/extensions_api/models/request.py
@@ -0,0 +1,178 @@
+"""
+Copyright 2013 Rackspace
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import json
+import xml.etree.ElementTree as ET
+
+from cafe.engine.models.base import AutoMarshallingModel
+from cloudcafe.compute.common.constants import Constants
+from cloudcafe.compute.common.models.metadata import Metadata
+from cloudcafe.compute.servers_api.models.requests import Personality
+
+
+class CreateServerFromVolume(AutoMarshallingModel):
+
+    def __init__(self, name, flavor_ref, block_device_mapping_v2,
+                 max_count=None, min_count=None, networks=None,
+                 image_ref=None, personality=None, user_data=None,
+                 metadata=None, accessIPv4=None, accessIPv6=None,
+                 disk_config=None, admin_pass=None, key_name=None,
+                 config_drive=None, scheduler_hints=None):
+
+        super(CreateServerFromVolume, self).__init__()
+        self.name = name
+        self.flavor_ref = flavor_ref
+        self.block_device_mapping_v2 = block_device_mapping_v2
+        self.max_count = max_count
+        self.min_count = min_count
+        self.networks = networks
+        self.image_ref = image_ref
+        self.personality = personality
+        self.user_data = user_data
+        self.metadata = metadata
+        self.accessIPv4 = accessIPv4
+        self.accessIPv6 = accessIPv6
+        self.disk_config = disk_config
+        self.admin_pass = admin_pass
+        self.key_name = key_name
+        self.config_drive = config_drive
+        self.scheduler_hints = scheduler_hints
+
+    def _obj_to_json(self):
+        body = {
+            'name': self.name,
+            'flavorRef': self.flavor_ref,
+            'block_device_mapping_v2': self.block_device_mapping_v2,
+            'max_count': self.max_count,
+            'min_count': self.min_count,
+            'networks': self.networks,
+            'imageRef': self.image_ref,
+            'personality': self.personality,
+            'user_data': self.user_data,
+            'metadata': self.metadata,
+            'accessIPv4': self.accessIPv4,
+            'accessIPv6': self.accessIPv6,
+            'OS-DCF:diskConfig': self.disk_config,
+            'adminPass': self.admin_pass,
+            'key_name': self.key_name,
+            'config_drive': self.config_drive
+        }
+
+        body = self._remove_empty_values(body)
+        main_body = {'server': body}
+
+        if self.scheduler_hints:
+            main_body['os:scheduler_hints'] = self.scheduler_hints
+
+        return json.dumps(main_body)
+
+    def _obj_to_xml(self):
+        element = ET.Element('server')
+        xml = Constants.XML_HEADER
+        element.set('xmlns', Constants.XML_API_NAMESPACE)
+        element.set('name', self.name)
+        element.set('flavorRef', self.flavor_ref)
+        block_device_ele = ET.Element('block_device_mapping_v2')
+        block_device_ele.append(BlockDeviceMappingV2._obj_to_xml(
+            self.block_device_mapping_v2))
+        element.append(block_device_ele)
+        if self.max_count is not None:
+            element.set('max_count', self.max_count)
+        if self.min_count is not None:
+            element.set('min_count', self.min_count)
+        if self.networks is not None:
+            networks_ele = ET.Element('networks')
+            for network_id in self.networks:
+                network = ET.Element('network')
+                network.set('uuid', network_id['uuid'])
+                networks_ele.append(network)
+            element.append(networks_ele)
+        if self.image_ref is not None:
+            element.set('image_ref', self.image_ref)
+        if self.personality is not None:
+            personality_ele = ET.Element('personality')
+            personality_ele.append(Personality._obj_to_xml(self.personality))
+            element.append(personality_ele)
+        if self.user_data is not None:
+            element.set('user_data', self.user_data)
+        if self.metadata is not None:
+            meta_ele = ET.Element('metadata')
+            for key, value in self.metadata.items():
+                meta_ele.append(Metadata._dict_to_xml(key, value))
+            element.append(meta_ele)
+        if self.accessIPv4 is not None:
+            element.set('accessIPv4', self.accessIPv4)
+        if self.accessIPv6 is not None:
+            element.set('accessIPv6', self.accessIPv6)
+        if self.disk_config is not None:
+            element.set('xmlns:OS-DCF',
+                        Constants.XML_API_DISK_CONFIG_NAMESPACE)
+            element.set('OS-DCF:diskConfig', self.disk_config)
+        if self.admin_pass is not None:
+            element.set('adminPass', self.admin_pass)
+        if self.key_name is not None:
+            element.set('key_name', self.key_name)
+        if self.config_drive is not None:
+            element.set('config_drive', self.config_drive)
+        xml += ET.tostring(element)
+        return xml
+
+
+class BlockDeviceMappingV2(AutoMarshallingModel):
+    """
+    @summary: Block Device Mapping Request Object for Version 2 Havana
+     of boot from volume extension
+    """
+    ROOT_TAG = 'block_device_mapping_v2'
+
+    def __init__(self, boot_index, uuid, volume_size, source_type,
+                 destination_type, delete_on_termination):
+        super(BlockDeviceMappingV2, self).__init__()
+        self.boot_index = boot_index
+        self.uuid = uuid
+        self.volume_size = volume_size
+        self.source_type = source_type
+        self.destination_type = destination_type
+        self.delete_on_termination = delete_on_termination
+
+    @classmethod
+    def _obj_to_json(self):
+        body = {
+            'boot_index': self.boot_index,
+            'uuid': self.uuid,
+            'volume_size': self.volume_size,
+            'source_type': self.source_type,
+            'destination_type': self.destination_type,
+            'delete_on_termination': self.delete_on_termination
+        }
+
+        body = self._remove_empty_values(body)
+        return json.dumps({'block_device_mapping_v2': body})
+
+    @classmethod
+    def _obj_to_xml(self, list_dicts):
+        device_dict = None
+        for device_dict in list_dicts:
+            device_element = ET.Element('block_device_mapping_v2')
+            device_element.set('boot_index', device_dict.get('boot_index'))
+            device_element.set('uuid', device_dict.get('uuid'))
+            device_element.set('volume_size', device_dict.get('volume_size'))
+            device_element.set('source_type', device_dict.get('source_type'))
+            device_element.set('destination_type',
+                               device_dict.get('destination_type'))
+            device_element.set('delete_on_termination',
+                               device_dict.get('delete_on_termination'))
+        return device_element
