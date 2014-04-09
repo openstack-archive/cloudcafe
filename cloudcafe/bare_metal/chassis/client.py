466391e0305b8adc18cb91c6f3e98faf886399fa
Daryl Walleck <daryl.walleck@RACKSPACE.COM>
2014-04-09 13:53:12 -0500

    Adds basic client methods for Ironic
    * Added basic API operations for chassis, drivers, nodes,
      and ports
    
    Change-Id: Id1cea2a1a4eb1557b4957f5b3dddeede1aea3adb

--- cloudcafe/bare_metal/chassis/client.py
+++ cloudcafe/bare_metal/chassis/client.py
@@ -13,3 +13,109 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+from cafe.engine.http.client import AutoMarshallingHTTPClient
+
+from cloudcafe.bare_metal.chassis.models.responses import Chassis, ChassisList
+from cloudcafe.bare_metal.chassis.models.requests import CreateChassis
+from cloudcafe.bare_metal.nodes.models.responses import Nodes
+
+
+class ChassisClient(AutoMarshallingHTTPClient):
+
+    def __init__(
+            self, url, auth_token, serialize_format=None,
+            deserialize_format=None):
+
+        super(ChassisClient, self).__init__(
+            serialize_format, deserialize_format)
+
+        self.url = url
+        self.auth_token = auth_token
+        self.default_headers['X-Auth-Token'] = auth_token
+        self.default_headers['Content-Type'] = 'application/{0}'.format(
+            self.serialize_format)
+        self.default_headers['Accept'] = 'application/{0}'.format(
+            self.deserialize_format)
+
+    def list_chassis(self, requestslib_kwargs=None):
+        """
+        @summary: Lists all chassis.
+        @return: resp
+        @rtype: Requests.response
+        """
+        url = '{base_url}/chassis'.format(base_url=self.url)
+        resp = self.get(url, response_entity_type=ChassisList,
+                        requestslib_kwargs=requestslib_kwargs)
+        return resp
+
+    def list_chassis_with_details(self, requestslib_kwargs=None):
+        """
+        @summary: Lists all chassis with details.
+        @return: resp
+        @rtype: Requests.response
+        """
+        url = '{base_url}/chassis/detail'.format(base_url=self.url)
+        resp = self.get(url, response_entity_type=ChassisList,
+                        requestslib_kwargs=requestslib_kwargs)
+        return resp
+
+    def list_nodes_for_chassis(self, uuid, requestslib_kwargs=None):
+        """
+        @summary: Lists all nodes associated with a chassis.
+        @return: resp
+        @rtype: Requests.response
+        """
+        url = '{base_url}/chassis/{chassis}/nodes'.format(
+            base_url=self.url, chassis=uuid)
+        resp = self.get(url, response_entity_type=Nodes,
+                        requestslib_kwargs=requestslib_kwargs)
+        return resp
+
+    def get_chassis(self, uuid, requestslib_kwargs=None):
+        """
+        @summary: Retrieves the details of an individual chassis.
+        @param uuid: The uuid of an existing chassis
+        @type uuid: String
+        @return: resp
+        @rtype: Requests.response
+        """
+        url = '{base_url}/chassis/{uuid}'.format(
+            base_url=self.url, uuid=uuid)
+        resp = self.get(url, response_entity_type=Chassis,
+                        requestslib_kwargs=requestslib_kwargs)
+        return resp
+
+    def create_chassis(self, description=None, extra=None,
+                       requestslib_kwargs=None):
+        """
+        @summary: Creates a chassis from the provided parameters.
+        @param description: Description of the chassis
+        @type description: String
+        @param description: Extra metadata for the chassis
+        @type description: Dict
+        @return: resp
+        @rtype: Requests.response
+        """
+        request = CreateChassis(
+            description=description, extra=extra)
+
+        url = '{base_url}/chassis'.format(base_url=self.url)
+        resp = self.post(url,
+                         response_entity_type=Chassis,
+                         request_entity=request,
+                         requestslib_kwargs=requestslib_kwargs)
+        return resp
+
+    def delete_chassis(self, uuid, requestslib_kwargs=None):
+        """
+        @summary: Deletes the specified chassis.
+        @param uuid: The uuid of a chassis
+        @type uuid: String
+        @return: resp
+        @rtype: Requests.response
+        """
+        url = '{base_url}/chassis/{uuid}'.format(
+            base_url=self.url, uuid=uuid)
+        resp = self.delete(url, requestslib_kwargs=requestslib_kwargs)
+        return resp
--- cloudcafe/bare_metal/drivers/client.py
+++ cloudcafe/bare_metal/drivers/client.py
@@ -13,3 +13,50 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+
+from cafe.engine.http.client import AutoMarshallingHTTPClient
+
+from cloudcafe.bare_metal.drivers.models.responses import Driver, Drivers
+
+
+class DriversClient(AutoMarshallingHTTPClient):
+
+    def __init__(
+            self, url, auth_token, serialize_format=None,
+            deserialize_format=None):
+
+        super(DriversClient, self).__init__(
+            serialize_format, deserialize_format)
+
+        self.url = url
+        self.auth_token = auth_token
+        self.default_headers['X-Auth-Token'] = auth_token
+        self.default_headers['Content-Type'] = 'application/{0}'.format(
+            self.serialize_format)
+        self.default_headers['Accept'] = 'application/{0}'.format(
+            self.deserialize_format)
+
+    def list_drivers(self, requestslib_kwargs=None):
+        """
+        @summary: Lists all drivers with details.
+        @return: resp
+        @rtype: Requests.response
+        """
+        url = '{base_url}/drivers'.format(base_url=self.url)
+        resp = self.get(url, response_entity_type=Drivers,
+                        requestslib_kwargs=requestslib_kwargs)
+        return resp
+
+    def get_driver(self, name, requestslib_kwargs=None):
+        """
+        @summary: Retrieves the details of an individual driver.
+        @param name: The uuid of an existing port
+        @type name: String
+        @return: resp
+        @rtype: Requests.response
+        """
+        url = '{base_url}/drivers/{uuid}'.format(
+            base_url=self.url, uuid=name)
+        resp = self.get(url, response_entity_type=Driver,
+                        requestslib_kwargs=requestslib_kwargs)
+        return resp
