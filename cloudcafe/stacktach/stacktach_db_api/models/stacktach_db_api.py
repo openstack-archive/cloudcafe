2b3d78d1cb7b4850db35b29c88e727cced0c2dfe
Priyanka Agrawal <priyanka.agrawal@RACKSPACE.COM>
2014-04-22 19:21:47 +0530

    Added stacktach images models
    * Added ImageExists, ImageUsages and ImageDeletes models
    for testing glance related functionalities in stacktach
    
    Change-Id: Ibc721cb0ccfe0f743b20e2b0a8736dd807eb69b1

--- cloudcafe/stacktach/stacktach_db_api/models/stacktach_db_api.py
+++ cloudcafe/stacktach/stacktach_db_api/models/stacktach_db_api.py
@@ -213,3 +213,175 @@ class ServerExists(AutoMarshallingListModel):
         exists.extend([ServerExist._dict_to_obj(exist)
                       for exist in exist_dict_list])
         return exists
+
+
+class ImageUsage(AutoMarshallingModel):
+
+    def __init__(self, id_=None, uuid=None, created_at=None, owner=None,
+                 size=None, last_raw_id=None):
+        """An object that represents a Image Usage in Stacktach DB."""
+        self.id_ = id_
+        self.uuid = uuid
+        self.created_at = created_at
+        self.owner = owner
+        self.size = size
+        self.last_raw_id = last_raw_id
+
+    def __repr__(self):
+        values = []
+        for prop, value in self.__dict__.items():
+            values.append("{0}: {1}".format(prop, value))
+        return "image: [{0}]".format(', '.join(values))
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        """
+        Returns an instance of a ImageUsage based on the json serialized_str
+        passed in.
+        """
+        image_dict = json.loads(serialized_str)
+        image = cls._dict_to_obj(image_dict)
+        return image
+
+    @classmethod
+    def _dict_to_obj(cls, image_dict):
+        """
+        Helper method to turn dictionary into ImageUsage instance.
+        """
+        if 'id' in image_dict:
+            image_dict['id_'] = image_dict.pop('id')
+        image = ImageUsage(**image_dict)
+        return image
+
+
+class ImageUsages(AutoMarshallingListModel):
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+        return cls._list_to_obj(json_dict.get('images'))
+
+    @classmethod
+    def _list_to_obj(cls, image_dict_list):
+        images = ImageUsages()
+        images.extend([ImageUsage._dict_to_obj(image)
+                      for image in image_dict_list])
+
+
+class ImageDelete(AutoMarshallingModel):
+    def __init__(self, id_=None, uuid=None, deleted_at=None, raw_id=None):
+        """An object that represents a Image Delete in Stacktach DB."""
+        super(ImageDelete, self).__init__()
+        self.id_ = id_
+        self.uuid = uuid
+        self.deleted_at = deleted_at
+        self.raw_id = raw_id
+
+    def __repr__(self):
+        values = []
+        for prop, value in self.__dict__.items():
+            values.append("{0}: {1}".format(prop, value))
+        return "delete: [{0}]".format(', '.join(values))
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        """
+        Returns an object of a Delete based on the json serialized_str
+        passed in.
+        """
+        json_dict = json.loads(serialized_str)
+        delete = cls._dict_to_obj(json_dict['delete'])
+        return delete
+
+    @classmethod
+    def _dict_to_obj(cls, delete_dict):
+        """
+        Helper method to turn dictionary into ImageDelete instance.
+        """
+        if 'id' in delete_dict:
+            delete_dict['id_'] = delete_dict.pop('id')
+        delete = ImageDelete(**delete_dict)
+        return delete
+
+
+class ImageDeletes(AutoMarshallingListModel):
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+        return cls._list_to_obj(json_dict.get('deletes'))
+
+    @classmethod
+    def _list_to_obj(cls, delete_dict_list):
+        deletes = ImageDeletes()
+        deletes.extend([ImageDelete._dict_to_obj(delete)
+                        for delete in delete_dict_list])
+
+
+class ImageExist(AutoMarshallingModel):
+    def __init__(self, id_=None, uuid=None, created_at=None,
+                 deleted_at=None, audit_period_beginning=None,
+                 audit_period_ending=None, status=None, fail_reason=None,
+                 raw_id=None, usage_id=None, delete_id=None, send_status=None,
+                 owner=None, size=None, message_id=None, event_id=None):
+        """
+        An object that represents a Image Exist in Stacktach DB.
+
+        """
+        self.id_ = id_
+        self.uuid = uuid
+        self.created_at = created_at
+        self.deleted_at = deleted_at
+        self.audit_period_beginning = audit_period_beginning
+        self.audit_period_ending = audit_period_ending
+        self.status = status
+        self.fail_reason = fail_reason
+        self.raw_id = raw_id
+        self.usage_id = usage_id
+        self.delete_id = delete_id
+        self.send_status = send_status
+        self.owner = owner
+        self.size = size
+        self.message_id = message_id
+        self.event_id = event_id
+
+    def __repr__(self):
+        values = []
+        for prop, value in self.__dict__.items():
+            values.append("{0}: {1}".format(prop, value))
+        return "exist: [{0}]".format(', '.join(values))
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        """
+        Returns an instance of an ImageExist based on the json serialized_str
+        passed in.
+        """
+        json_dict = json.loads(serialized_str)
+        exist = cls._dict_to_obj(json_dict['exist'])
+        return exist
+
+    @classmethod
+    def _dict_to_obj(cls, exist_dict):
+        """
+        Helper method to turn dictionary into ImageExist instance.
+        """
+        if 'id' in exist_dict:
+            exist_dict['id_'] = exist_dict.pop('id')
+        exist = ImageExist(**exist_dict)
+        return exist
+
+
+class ImageExists(AutoMarshallingListModel):
+
+    @classmethod
+    def _json_to_obj(cls, serialized_string):
+        json_dict = json.loads(serialized_string)
+        return cls._list_to_obj(json_dict.get('exists'))
+
+    @classmethod
+    def _list_to_obj(cls, exist_dict_list):
+        exists = ImageExists()
+        exists.extend([ImageExist._dict_to_obj(exist)
+                       for exist in exist_dict_list])
+        return exists
