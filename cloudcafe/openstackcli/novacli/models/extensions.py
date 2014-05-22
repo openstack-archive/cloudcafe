4b41cd5aee5bacfa49171fa21e6c9b3dec069224
Jose Idar <jose.idar@rackspace.com>
2014-05-22 14:03:34 -0500

    Update of openstackcli common and nova components.
     * Added support for the following nova commands to
       the nova client:
         boot, show, list, image-list, image-show,
         flavor-list, flavor-show, volume-attach,
         volume-create, volume-delete, volume-detach,
         volume-show, volume-snapshot-create,
         volume-snapshot-delete, volume-snapshot-list,
         volume-snapshot-show, volume-type-create,
         volume-type-delete, volume-type-list.
     * Added response models and extension support for
       all existing and new nova client methods.
     * Added a novacli composite.
     * Added environment variable dictionary support to
       the nova client config.
     * Added new 'Simple' base response models to
       cover deserialization of the most common pretty
       table type.
     * Added new unified response extension base class,
       deprecating the old one.
     * Fixes docstring and comment spacing.
    
    Change-Id: I8183a0b8f502496af50eb06d3896cb9ac325ef0e

--- cloudcafe/openstackcli/novacli/models/extensions.py
+++ cloudcafe/openstackcli/novacli/models/extensions.py
@@ -1,51 +1,78 @@
-from cloudcafe.openstackcli.common.models.extensions import \
-    ResponseExtensionType, SingleAttributeResponseExtension
+from cloudcafe.openstackcli.common.models.extensions import (
+    SimpleResponseExtension, AttributeAggregatingResponseExtension)
 
 # Extensions defined here are registered in this list
 extensions = []
 
 
-class OS_DCF_show(SingleAttributeResponseExtension):
-    __extends__ = 'ServerResponse'
-    key_name = 'OS-DCF:diskConfig'
-    attr_name = 'disk_config'
+class ConfigDrive(SimpleResponseExtension):
+    __extends__ = ['ServerResponse']
+    _sub_attr_map = {'config_drive': 'config_drive'}
 
 
-class ConfigDrive(SingleAttributeResponseExtension):
-    __extends__ = 'ServerResponse'
-    key_name = 'config_drive'
-    attr_name = 'config_drive'
-
-
-class OS_EXT_STS_show(object):
-    __metaclass__ = ResponseExtensionType
-    __extends__ = 'ServerResponse'
-    _prefix = 'OS-EXT-STS'
+class OS_EXT_STS_show(SimpleResponseExtension):
+    __extends__ = ['ServerResponse']
     _sub_attr_map = {
-        'OS-EXT-STS:power_state': 'power_state',
-        'OS-EXT-STS:task_state': 'task_state',
-        'OS-EXT-STS:vm_state': 'vm_state'}
-
-    def extend(cls, obj, **kwargs):
-        if obj.__class__.__name__ not in cls.__extends__:
-            return obj
-
-        for kw_name, attr_name in cls._sub_attr_map.items():
-            setattr(obj, attr_name, kwargs.get(kw_name, None))
-        return obj
+        'OS-EXT-STS:power_state': 'os_ext_sts_power_state',
+        'OS-EXT-STS:task_state': 'os_ext_sts_task_state',
+        'OS-EXT-STS:vm_state': 'os_ext_sts_vm_state'}
 
 
-class OS_EXT_STS_list(object):
-    __metaclass__ = ResponseExtensionType
-    __extends__ = '_ServerListItem'
+class OS_EXT_STS_list(SimpleResponseExtension):
+    __extends__ = ['_ServerListItem']
     _sub_attr_map = {
         'Power State': 'power_state',
         'Task State': 'task_state'}
 
-    def extend(cls, obj, **kwargs):
-        if obj.__class__.__name__ not in cls.__extends__:
-            return obj
 
-        for kw_name, attr_name in cls._sub_attr_map.items():
-            setattr(obj, attr_name, kwargs.get(kw_name, None))
-        return obj
+class OS_DCF_show(SimpleResponseExtension):
+    __extends__ = ['ServerResponse', 'ImageShowResponse']
+    _sub_attr_map = {'OS-DCF:diskConfig': 'os_dcf_disk_config'}
+
+
+class OS_EXT_IMG_SIZE(SimpleResponseExtension):
+    __extends__ = ['ImageShowResponse']
+    _sub_attr_map = {'OS-EXT-STS:size': 'os_ext_img_size'}
+
+
+class image_show_metadata_aggregator(AttributeAggregatingResponseExtension):
+    __extends__ = ["ImageShowResponse"]
+    _prefix = "metadata "  # The space after metadata is intentional
+    _new_dict_attribute_name = "metadata"
+
+
+class OS_FLV_EXT_DATA(SimpleResponseExtension):
+    __extends__ = ['FlavorShowResponse']
+    _sub_attr_map = {
+        'OS-FLV-EXT-DATA:ephemeral': 'os_flv_ext_data_ephemeral'}
+
+
+class OS_FLV_WITH_EXT_SPECS(SimpleResponseExtension):
+    __extends__ = ['FlavorShowResponse']
+    _sub_attr_map = {
+        'OS-FLV-WITH-EXT-SPECS:extra_specs':
+        'os_flv_with_ext_specs_extra_specs'}
+
+
+class os_vol_mig_status_attr(SimpleResponseExtension):
+    __extends__ = ['VolumeCreateResponse']
+    _prefix = 'os-vol-mig-status-attr'
+    _sub_attr_map = {
+        'os-vol-mig-status-attr:migstat': 'os_vol_mig_status_attr_migstat',
+        'os-vol-mig-status-attr:name_id': 'os_vol_mig_status_attr_name_id'}
+
+
+class os_vol_tenant_attr(SimpleResponseExtension):
+    __extends__ = ['VolumeCreateResponse']
+    _sub_attr_map = {
+        'os-vol-tenant-attr:tenant_id': 'os_vol_tenant_attr_tenant_id'}
+
+
+class os_extended_snapshot_attributes(SimpleResponseExtension):
+    __extends__ = ['VolumeSnapshotShowResponse']
+    _sub_attr_map = {
+        'os-extended-snapshot-attributes:progress':
+        'os_extended_snapshot_attributes_progress',
+
+        'os-extended-snapshot-attributes:project_id':
+        'os_extended_snapshot_attributes_project_id'}
