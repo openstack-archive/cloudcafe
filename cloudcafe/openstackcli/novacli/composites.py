b22a44f4862649b872038b122cc531b5179dd4e4
Jose Idar <jose.idar@rackspace.com>
2014-05-22 14:03:34 -0500

    Update of openstackcli command and nova components.
     * Added support for the following nova commands to
       the nova client:
         boot, show, list, image-list, image-show,
         flavor-list, flavor-show, volume-attach,
         volume-create, volume-delete, volume-detach,
         volume-show, volume-snapshot-create,
         volume-snapshot-delete, volume-snapshot-list,
         volume-snapshot-show, volume-type-create,
         volume-type-delete, volume-type-list.
     * Added response models and extension support for
       all existing and new nova client methods.
     * Added a novacli composite.
     * Added environment variable dictionary support to
       the nova client config.
     * Added new 'Simple' base response models to
       cover deserialization of the most common pretty
       table type.
     * Added new unified response extension base class,
       deprecating the old one.
     * Fixes docstring and comment spacing.
    
    Change-Id: I8183a0b8f502496af50eb06d3896cb9ac325ef0e

---
+++ cloudcafe/openstackcli/novacli/composites.py
@@ -0,0 +1,24 @@
+from cloudcafe.openstackcli.novacli.client import NovaCLI
+from cloudcafe.openstackcli.common.config import OpenstackCLI_CommonConfig
+from cloudcafe.openstackcli.novacli.config import NovaCLI_Config
+
+
+class NovaCLI_Composite(object):
+
+    def __init__(self):
+        self.openstack_config = OpenstackCLI_CommonConfig()
+        self.config = NovaCLI_Config()
+        self._cli_kwargs = {
+            'os_username': self.openstack_config.os_username,
+            'os_password': self.openstack_config.os_password,
+            'os_tenant_name': self.openstack_config.os_tenant_name,
+            'os_auth_url': self.openstack_config.os_auth_url,
+            'os_region_name': self.openstack_config.os_region_name,
+            'debug': self.openstack_config.debug,
+            'os_auth_system': self.config.os_auth_system,
+            'insecure': self.config.insecure}
+        self.client = NovaCLI(**self._cli_kwargs)
+        self.client.set_environment_variables(
+            self.config.environment_variable_dictionary)
+        import os
+        print os.environ
