c9688d4c189a9a7f2d7e9e79c5e443fadaa83fd5
Daryl Walleck <daryl.walleck@rackspace.com>
2014-09-24 15:47:35 -0500

    Start of Compute Behaviors refactor
    * Broke create_active_server into parts: create_server_with_defaults
      and wait_for_server_creation
    * Implemented StatusProgressionVerifier for server builds in
      wait_for_server_creation
    * Added a base Compute behavior with a verify_entity method to handle
      basic response checks
    * Removed retries for server builds
    * Removed configuration for server build retries
    * Fixed a minor unrelated pep8 issue in the servers behavior
    
    Change-Id: I9cd9bb346edb76e3a5a6b72019b553884f1d18ca

--- cloudcafe/compute/extensions/config_drive/behaviors.py
+++ cloudcafe/compute/extensions/config_drive/behaviors.py
@@ -14,14 +14,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from cafe.engine.behaviors import BaseBehavior
+from cloudcafe.compute.common.behaviors import BaseComputeBehavior
 from cloudcafe.compute.extensions.config_drive.models.\
     config_drive_openstack_meta import OpenStackMeta
 from cloudcafe.compute.extensions.config_drive.models.\
     config_drive_ec_metadata import EcMetadata
 
 
-class ConfigDriveBehaviors(BaseBehavior):
+class ConfigDriveBehaviors(BaseComputeBehavior):
 
     def __init__(self, servers_client, servers_config,
                  server_behaviors):
