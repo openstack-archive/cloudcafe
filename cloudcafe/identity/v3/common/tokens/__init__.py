2414a543f764920d4d75dd20eb0c5487443e413e
Shanthi <shanthi.vedula@rackspace.com>
2014-11-11 09:37:14 -0600

    Modified the v3 composite based on the discussion with the team
    * Dependant on pr: https://review.openstack.org/#/c/135339/
    * Split the composite to include version specific composite and the base composite
    
    Fixed the copyright conflict
    
    Added the common composite and the tokens behavior for v3
    * Added the init files with the copyright
    * Added the composite for v3
    * Added the tokens behavior for v3
    
    Change-Id: If5be6fd41148628608e6e273dc8a94ff7df59fac

--- cloudcafe/identity/v3/common/tokens/__init__.py
+++ cloudcafe/identity/v3/common/tokens/__init__.py
@@ -1,4 +1,4 @@
-from cloudcafe.identity.v3.common.tokens.client import TokensClient
 from cloudcafe.identity.v3.common.tokens.behavior import TokensBehavior
+from cloudcafe.identity.v3.common.tokens.client import TokensClient
 client = TokensClient
 behavior = TokensBehavior
