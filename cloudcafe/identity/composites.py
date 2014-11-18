9d3d235b8e63b9cb031d49dc1d1630118ec84553
Shanthi <shanthi.vedula@rackspace.com>
2014-11-18 10:18:25 -0600

    Added composite and config
    * Added the base composite for v3, v2, ..
    * Added the common config
    
    Change-Id: I3dba4eb5bf7394d1aa964d42a4851f15eb153a83

--- cloudcafe/identity/composites.py
+++ cloudcafe/identity/composites.py
@@ -1,97 +1,112 @@
-from cloudcafe.identity.v2_0.behaviors import IdentityServiceBehaviors
-from cloudcafe.identity.v2_0.client import IdentityServiceClient
-from cloudcafe.identity.v2_0.config import (
-    IdentityConfig, IdentityUserConfig, IdentityAdminConfig)
-from cloudcafe.identity.v2_0.extensions import osksadm
+import cloudcafe.identity as lib_pt
+import fnmatch
+import importlib
+import os
+
+from cloudcafe.identity.common.client import BaseIdentityAPIClient
+from cloudcafe.identity.config import IdentityConfig
+from cloudcafe.identity.v3.config import (
+    ServiceAdmin, UserAdmin, DefaultUser, Roles)
 
 
-class IdentityServiceComposite(object):
-    _user_config_class = IdentityUserConfig
-
-    def __init__(self):
-        ident_config = IdentityConfig()
-        user_config = self._user_config_class()
-        self.serialize_format = ident_config.serialize_format
-        self.deserialize_format = ident_config.deserialize_format
-        self.config_token = ident_config.token_id
-        self.service_name = ident_config.identity_service_name
-        self.region = ident_config.region
-        self.username = user_config.username
-        self.password = user_config.password
-        self.tenant_name = user_config.tenant_name
-        self.config_tenant_id = user_config.tenant_id
-        self.endpoint = user_config.authentication_endpoint
-
-        self.client = IdentityServiceClient(
-            url=self.endpoint, serialize_format=self.serialize_format,
-            deserialize_format=self.deserialize_format)
-        self.behaviors = IdentityServiceBehaviors(
-            self.client)
-        self.public_url = None
-        self.private_url = None
-        self.admin_url = None
-
-    def authenticate(self):
-        resp = IdentityServiceBehaviors.memoized_authenticate(
-            self.username, self.password, self.tenant_name, self.client.url,
-            self.serialize_format, self.deserialize_format)
-        if resp.entity is None:
-            raise Exception("Failed to authenticate")
-        self.access_data = resp.entity
-        self.client.token = self.access_data.token.id_
-        self.user_id = self.access_data.user.id_
-        self.tenant_id = self.access_data.token.tenant.id_
-        service = self.access_data.get_service(self.service_name)
-        if service:
-            endpoint = service.get_endpoint(self.region)
-            self.public_url = endpoint.public_url
-            self.private_url = endpoint.private_url
-            self.admin_url = endpoint.admin_url
-
-    def load_extensions(self):
-        self.extensions = ExtensionsComposite(self.endpoint, self.client.token)
+class IdentityVersionNotSet(Exception):
+    pass
 
 
-class AdminIdentityServiceComposite(IdentityServiceComposite):
-    _user_config_class = IdentityAdminConfig
+class IdentityComposite(object):
+    _user_config_class = UserAdmin
+    _ident_config_class = IdentityConfig
+
+    def __init__(self, user_config=None):
+        self.version = None
+        self.ident_config = self._ident_config_class()
+        self.service_admin = ServiceAdmin()
+        self.default_user = DefaultUser()
+        self.roles = Roles()
+        self.user_config = (user_config() if user_config else
+                            self._user_config_class())
+
+        self.user_type = self.user_config.SECTION_NAME
+        self.url = (
+            self.user_config.authentication_endpoint or
+            self.ident_config.global_authentication_endpoint)
+
+    def load_clients_and_behaviors(self):
+        """
+        @summary Recursively loads client and behaviors as specified in the
+        __init__.py files.
+        """
+        self.fetch_token()
+        kwargs = {
+            "url": self.url,
+            "serialize_format": self.ident_config.serialize_format,
+            "deserialize_format": self.ident_config.deserialize_format,
+            "auth_token": self.token}
+        self.apis = APINamespace()
+
+        # If the version is not specified or it not the correct version
+        # throw an error
+        if self.version is None:
+            msg = "Version must be set"
+            raise IdentityVersionNotSet(msg)
+
+        try:
+            importlib.import_module(
+                "cloudcafe.identity.{0}".format(self.version))
+        except:
+            raise ImportError("No version {0} found".format(self.version))
+
+        # get_modules_within will get the __init__.py's from the
+        # subdirectories. It then loads those and gets the client or behavior
+        # that is set within the __init__.py
+        for name, module in self.get_modules_within(lib_pt).iteritems():
+            module_path = "{path}.{module}".format(
+                path=lib_pt.__name__, module=module)
+            module = module.replace("{0}.common.".format(self.version), "")
+            package = importlib.import_module(module_path)
+            client_class = getattr(package, "client", None)
+            behavior_class = getattr(package, "behavior", None)
+            if (not isinstance(client_class, type) or
+                    not issubclass(client_class, BaseIdentityAPIClient)):
+                continue
+            client = client_class(**kwargs)
+            setattr(self.apis, module, APIComposite(client, behavior_class))
+
+    def fetch_token(self):
+        pass
+
+    def get_modules_within(self, library):
+        """
+        @summary This method finds all submodules and returns a dictionary
+        For example common/catalog/client.py would be become this map:
+        catalog : common.catalog.client
+        """
+        path = os.path.dirname(library.__file__)
+        file_type = "__init__.py"
+
+        # This will loop through all subdirectories of the specified
+        # module for __init__.py files
+        modules = [os.path.join(dirpath, f)
+                   for dirpath, dirnames, files in os.walk(path)
+                   for f in fnmatch.filter(files, file_type)]
+
+        # get the module names + .py from the path, then remove the .py
+        modules = [x.split(path)[1].lstrip('/') for x in modules if path in x]
+        modules = [x.strip(file_type) for x in modules]
+
+        # convert the forward slashes to .'s
+        modules = [x.rstrip('/').replace('/', '.') for x in modules if x != '']
+
+        # build up a hash list mapping module name to the module path
+        return dict([(x.split('.')[-1], x) for x in modules])
 
 
-class ExtensionsComposite():
-    extensions = {}
-
-    def __init__(self, url, auth_token):
-        for ext in self.extensions:
-            composite = self.extensions.get(ext)(url, auth_token)
-            setattr(self, ext, composite)
+class APINamespace(object):
+    pass
 
 
-class ExtensionType(type):
-    def __new__(cls, cls_name, cls_parents, cls_attr):
-        new_class = super(ExtensionType, cls).__new__(
-            cls, cls_name, cls_parents, cls_attr)
-        if getattr(new_class, "_name", False):
-            ExtensionsComposite.extensions[new_class._name] = new_class
-        return new_class
-
-
-class BaseIdentityExtensionComposite(object):
-    _client = None
-    _behaviors = None
-    _name = None
-    __metaclass__ = ExtensionType
-
-    def __init__(self, url, auth_token):
-        config = IdentityConfig()
-        self.client = self._client(
-            url=url, auth_token=auth_token,
-            serialize_format=config.serialize_format,
-            deserialize_format=config.deserialize_format)
-        if self._behaviors is not None:
-            self.behaviors = self._behaviors(self.client)
-
-
-#All Extension Composites below this point
-class OSKSADMComposite(BaseIdentityExtensionComposite):
-    _client = osksadm.client
-    _behaviors = osksadm.behaviors
-    _name = "osksadm"
+class APIComposite(object):
+    def __init__(self, client, behavior=None):
+        self.client = client
+        if behavior is not None:
+            self.behavior = behavior(client)
