5a7f84a158b72e9e3793721dc7a9f59be2b504df
Luke Wollney <luke.wollney@rackspace.com>
2014-12-16 16:08:01 -0600

    Updated Glance refactor
    *Updated copyright dates
    *Removed HTTP response codes constants that are no longer used
    
    Change-Id: I6daf532fd963c0611f6538f8ccd694f797b9b858

--- cloudcafe/glance/common/constants.py
+++ cloudcafe/glance/common/constants.py
@@ -1,5 +1,5 @@
 """
-Copyright 2013 Rackspace
+Copyright 2014 Rackspace
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,11 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-
-class HTTPResponseCodes(object):
-    OK = 200
-    FORBIDDEN = 403
 
 
 class ImageProperties(object):
