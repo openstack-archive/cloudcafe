c750e04ea68744bf824fa1d04c2355834d12cc6e
Levi Blackstone <levi.blackstone@RACKSPACE.COM>
2015-01-16 14:58:13 -0600

    Add additional common models for compute event notifications
    * Add models for Bandwidth, ImageMeta, and InstanceException
    * Add metatests for new models
    * Rename base metatest class
    
    Change-Id: I551184b12753481cd36db7950c5296ab9ce923e6

--- cloudcafe/events/models/compute/common.py
+++ cloudcafe/events/models/compute/common.py
@@ -17,6 +17,54 @@ limitations under the License.
 from cloudcafe.events.models.base import EventBaseModel, EventBaseListModel
 
 
+class Bandwidth(EventBaseModel):
+    """Bandwidth Response Model
+
+    @summary: Response model for bandwidth from a compute
+        event notification
+
+    JSON Example:
+        {
+            "private": { <BandwidthInterface> },
+            "public": { <BandwidthInterface> }
+        }
+    """
+    kwarg_map = {'private': 'private',
+                 'public': 'public'}
+
+    def __init__(self, private, public):
+        super(Bandwidth, self).__init__(locals())
+
+    @classmethod
+    def _dict_to_obj(cls, json_dict):
+        """Override dict_to_obj implementation"""
+        obj = cls._map_values_to_kwargs(json_dict)
+        obj.private = BandwidthInterface._dict_to_obj(obj.private)
+        obj.public = BandwidthInterface._dict_to_obj(obj.public)
+
+        return obj
+
+
+class BandwidthInterface(EventBaseModel):
+    """Bandwidth Interface Response Model
+
+    @summary: Response model for bandwidth on an interface from
+        a compute event notification
+    @note: Sub-model of Bandwidth
+
+    JSON Example:
+        {
+            "bw_in": 123456,
+            "bw_out": 654321
+        }
+    """
+    kwarg_map = {'bw_in': 'bw_in',
+                 'bw_out': 'bw_out'}
+
+    def __init__(self, bw_in, bw_out):
+        super(BandwidthInterface, self).__init__(locals())
+
+
 class FixedIp(EventBaseModel):
     """Fixed IP Response Model
 
@@ -66,3 +114,73 @@ class FixedIps(EventBaseListModel):
     """
     list_model_key = 'fixed_ips'
     ObjectModel = FixedIp
+
+
+class ImageMeta(EventBaseModel):
+    """Image Metadata Model
+
+    @summary: Response model for image metadata from a compute
+        event notification
+    @note: This type may contain additional unspecified
+        fields, which will be captured in a dictionary called kwargs
+
+    JSON Example:
+        {
+            "image_meta": {
+                "auto_disk_config": "disabled",
+                "base_image_ref": "5e91ad7f-afe4-4a83-bd5f-84673462cae1",
+                "container_format": "ovf",
+                "disk_format": "vhd",
+                "image_type": "base",
+                "min_disk": "20",
+                "min_ram": "512",
+                "org.openstack__1__architecture": "x64",
+                "org.openstack__1__os_distro": "com.ubuntu",
+                "org.openstack__1__os_version": "12.04",
+                "os_type": "linux"
+            }
+        }
+    """
+    kwarg_map = {
+        'auto_disk_config': 'auto_disk_config',
+        'base_image_ref': 'base_image_ref',
+        'container_format': 'container_format',
+        'disk_format': 'disk_format',
+        'image_type': 'image_type',
+        'min_disk': 'min_disk',
+        'min_ram': 'min_ram',
+        'org_openstack__1__architecture': 'org.openstack__1__architecture',
+        'org_openstack__1__os_distro': 'org.openstack__1__os_distro',
+        'org_openstack__1__os_version': 'org.openstack__1__os_version',
+        'os_type': 'os_type'}
+    strict_checking = False
+
+    def __init__(self, auto_disk_config, base_image_ref, container_format,
+                 disk_format, image_type, min_disk, min_ram,
+                 org_openstack__1__architecture, org_openstack__1__os_distro,
+                 org_openstack__1__os_version, os_type, **kwargs):
+        super(ImageMeta, self).__init__(locals())
+
+
+class InstanceException(EventBaseModel):
+    """Instance Exception Model
+
+    @summary: Response model for an instance exception from a
+        compute event notification
+    @note: Represents a single instance exception
+
+    JSON Example:
+        {
+            "exception": {
+                "kwargs": {
+                    "instance_uuid": "5e91ad7f-afe4-4a83-bd5f-84673462cae1",
+                    "reason": "Something broke",
+                    "code": 500
+                }
+            }
+        }
+    """
+    kwarg_map = {'kwargs': 'kwargs'}
+
+    def __init__(self, kwargs):
+        super(InstanceException, self).__init__(locals())
