8cf2190789520e0d42e37114664b98c90dd7feb7
Steve Heyman <steve.heyman@rackspace.com>
2015-11-12 10:59:10 -0600

    Updated models for latest Barbican/CloudKeep and removed unused portcullis
    Change-Id: I11e4975377190d75c2a4c4787427ea2c933be082

--- cloudcafe/cloudkeep/barbican/version/models/version.py
+++ cloudcafe/cloudkeep/barbican/version/models/version.py
@@ -15,29 +15,70 @@ limitations under the License.
 """
 
 import json
-from cafe.engine.models.base import AutoMarshallingModel
+
+from cafe.engine.models.base import (
+    AutoMarshallingModel, AutoMarshallingListModel)
+from cloudcafe.compute.common.equality_tools import EqualityTools
+from cloudcafe.images.common.models.links import Links
 
 
 class Version(AutoMarshallingModel):
-    def __init__(self, version, build):
+    """@summary: Version model"""
+
+    def __init__(self, api_id=None, links=None, status=None, updated=None,
+                 media_types=None):
         super(Version, self).__init__()
-        self.v1 = version
-        self.build = build
+        self.api_id = api_id
+        self.links = links
+        self.status = status
+        self.updated = updated
+        self.media_types = media_types
+
+    def __eq__(self, other):
+        return EqualityTools.are_objects_equal(self, other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     @classmethod
     def _json_to_obj(cls, serialized_str):
-        """Returns an instance of a Version based on the json serialized_str
-        passed in."""
-        result = None
-        json_obj = json.loads(serialized_str)
-        if json_obj is not None:
-            result = cls._dict_to_obj(json_obj)
-        return result
+        json_dict = json.loads(serialized_str)
+        return cls._dict_to_obj(json_dict)
 
     @classmethod
     def _dict_to_obj(cls, json_dict):
-        kwargs = {
-            'version': json_dict.get('v1'),
-            'build': json_dict.get('build')
-        }
-        return Version(**kwargs)
+        json_dict = json_dict['versions']['values'][0]
+        links = None
+        if 'links' in json_dict:
+            links = Links._list_to_obj(json_dict.get('links'))
+        version = Version(api_id=json_dict.get('id'), links=links,
+                          status=json_dict.get('status'),
+                          updated=json_dict.get('updated'),
+                          media_types=json_dict.get('media-types'))
+        return version
+
+    def _obj_to_json(self):
+        obj_dict = {}
+        obj_dict['id'] = self.api_id
+        obj_dict['links'] = self.links
+        obj_dict['status'] = self.status
+        return json.dumps(obj_dict)
+
+
+class Versions(AutoMarshallingListModel):
+    """@summary: Versions model"""
+
+    def __init__(self, versions=None):
+        self.extend(versions or [])
+
+    @classmethod
+    def _json_to_obj(cls, serialized_str):
+        json_dict = json.loads(serialized_str)
+        return cls._list_to_obj(json_dict.get('versions'))
+
+    @classmethod
+    def _list_to_obj(cls, dict_list):
+        versions = Versions()
+        for version_dict in dict_list:
+            versions.append(Version._dict_to_obj(version_dict))
+        return versions
